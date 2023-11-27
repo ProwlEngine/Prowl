031df6f173abb07a5a0fc59fa2379ca04e7927ff
Wulferis <acer1171090@gmail.com>
2023-11-28 02:03:49 +1100

    I cant help myself DOF needs to be stronger hehe

--- Prowl.Runtime/Components/ImageEffects/DOFEffect.cs
+++ Prowl.Runtime/Components/ImageEffects/DOFEffect.cs
@@ -8,7 +8,7 @@ namespace Prowl.Runtime.Components.ImageEffects
     [ExecuteAlways]
     public class DOFEffect : MonoBehaviour
     {
-        public float focusStrength = 40f;
+        public float focusStrength = 150f;
         public float quality = 0.05f;
         public int blurRadius = 10;
 
