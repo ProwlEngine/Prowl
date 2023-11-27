0f1db6d8f142b249ff31bbc825f7d954e81c9534
Wulferis <acer1171090@gmail.com>
2023-11-27 13:44:27 +1100

    Test Scriptable Object

---
+++ Prowl.Runtime/Resources/RenderPipeline.cs
@@ -0,0 +1,28 @@
+﻿using Prowl.Runtime.Utils;
+using System;
+
+namespace Prowl.Runtime.Resources
+{
+    [CreateAssetMenu("RenderPipeline")]
+    public class RenderPipeline : ScriptableObject
+    {
+        [Text("This Script is a text for ScriptableObjects")]
+        [Tooltip("This field is clamped [0-10] in OnValidate")]
+        public float testingFloat = 9f;
+
+        [Seperator()]
+        [Tooltip("This field does nothing :D")]
+        public bool ThisScriptIsATest = true;
+
+        public override void OnValidate()
+        {
+            testingFloat = Math.Clamp(testingFloat, 0f, 10f);
+        }
+
+        [ImGUIButton("ToggleTest")]
+        public void Toggle()
+        {
+            ThisScriptIsATest = !ThisScriptIsATest;
+        }
+    }
+}
