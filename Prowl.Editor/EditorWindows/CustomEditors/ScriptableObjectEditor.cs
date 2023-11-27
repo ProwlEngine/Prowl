22a385cba37b3dc0558267f413cfba1f4a14bd92
Wulferis <acer1171090@gmail.com>
2023-11-27 13:28:57 +1100

    ScriptableObject Custom Editor & OnValidate()

---
+++ Prowl.Editor/EditorWindows/CustomEditors/ScriptableObjectEditor.cs
@@ -0,0 +1,92 @@
+﻿using Prowl.Runtime;
+using Prowl.Runtime.Utils;
+using Prowl.Editor.Assets;
+using Prowl.Editor.PropertyDrawers;
+using HexaEngine.ImGuiNET;
+using System.Reflection;
+using Prowl.Runtime.Resources;
+using Prowl.Runtime.Assets;
+using System;
+
+namespace Prowl.Editor.EditorWindows.CustomEditors
+{
+    [CustomEditor(typeof(ScriptableObjectImporter))]
+    public class ScriptableObjectEditor : ScriptedEditor
+    {
+        public override void OnInspectorGUI()
+        {
+            var importer = (ScriptableObjectImporter)(target as MetaFile).importer;
+            ImGui.PushID(importer.GetHashCode());
+
+            try
+            {
+                bool changed = false;
+                ScriptableObject scriptObject = JsonUtility.Deserialize<ScriptableObject>(File.ReadAllText((target as MetaFile).AssetPath.FullName));
+
+                FieldInfo[] fields = scriptObject.GetType().GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+                // Private fields need to have the SerializeField attribute
+                fields = fields.Where(field => field.IsPublic || Attribute.IsDefined(field, typeof(SerializeFieldAttribute))).ToArray();
+
+                foreach (var field in fields)
+                {
+                    // Dont render if the field has the Hide attribute
+                    if (!Attribute.IsDefined(field, typeof(HideInInspectorAttribute)))
+                    {
+                        var attributes = field.GetCustomAttributes(true);
+                        var imGuiAttributes = attributes.Where(attr => attr is IImGUIAttri).Cast<IImGUIAttri>();
+
+                        foreach (var imGuiAttribute in imGuiAttributes)
+                            imGuiAttribute.Draw();
+
+                        // enums are a special case
+                        if (field.FieldType.IsEnum)
+                        {
+                            var currentEnumValue = (Enum)field.GetValue(scriptObject);
+
+                            if (ImGui.BeginCombo(field.FieldType.Name, currentEnumValue.ToString()))
+                            {
+                                foreach (var enumValue in Enum.GetValues(field.FieldType))
+                                {
+                                    bool isSelected = currentEnumValue.Equals(enumValue);
+
+                                    bool enumChanged = ImGui.Selectable(enumValue.ToString(), isSelected);
+                                    changed |= enumChanged;
+                                    if (enumChanged) field.SetValue(scriptObject, enumValue);
+
+                                    if (isSelected) ImGui.SetItemDefaultFocus();
+                                }
+
+                                ImGui.EndCombo();
+                            }
+                        }
+                        else
+                        {
+                            // Draw the field using PropertyDrawer.Draw
+                            changed |= PropertyDrawer.Draw(scriptObject, field);
+                        }
+
+                        foreach (var imGuiAttribute in imGuiAttributes)
+                            imGuiAttribute.End();
+                    }
+                }
+
+                // Draw any Buttons
+                ImGUIButtonAttribute.DrawButtons(scriptObject);
+
+                if (changed)
+                {
+                    scriptObject.OnValidate();
+                    File.WriteAllText((target as MetaFile).AssetPath.FullName, JsonUtility.Serialize(scriptObject));
+                    AssetDatabase.Reimport(AssetDatabase.FileToRelative((target as MetaFile).AssetPath));
+                }
+            }
+            catch
+            {
+                ImGui.LabelText("Failed to Deserialize ScriptableObject", "The ScriptableObject file is invalid.");
+            }
+            ImGui.PopID();
+        }
+
+    }
+}
--- Prowl.Runtime/Resources/ScriptableObject.cs
+++ Prowl.Runtime/Resources/ScriptableObject.cs
@@ -5,13 +5,7 @@
         public ScriptableObject() : base() { }
         public ScriptableObject(string name) : base(name) { }
 
-        public override void CreatedInstance()
-        {
-            base.CreatedInstance();
-            Awake();
-        }
-
-        public virtual void Awake() { }
+        public virtual void OnValidate() { }
 
     }
 }
