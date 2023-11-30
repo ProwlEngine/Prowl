ProwlEngine/Prowl

commit 5e0188576661df9e1c9ebc0516ded32a67bb6e97

Changed Camera.Render to include Resolution Parameters

Prowl.Runtime/Components/Camera.cs
@@ -32,7 +32,7 @@ public class Camera : MonoBehaviour
     public event Action<GBuffer> PostProcessStagePostCombine;
     public event Action<int, int> Resize;
 
-    public RenderTexture Target;
+    public AssetRef<RenderTexture> Target;
 
     public GBuffer gBuffer { get; private set; }
     Resources.Material CombineShader;
@@ -51,7 +51,7 @@ public class Camera : MonoBehaviour
 
     private Vector2 GetRenderTargetSize()
     {
-        if (Target != null) return new Vector2(Target.Width, Target.Height);
+        if (Target.IsAvailable) return new Vector2(Target.Res!.Width, Target.Res!.Height);
         return new Vector2(Raylib.GetScreenWidth(), Raylib.GetScreenHeight());
     }
 
@@ -122,14 +122,32 @@ public class Camera : MonoBehaviour
         gBuffer.EndCombine();
     }
 
-    public void Render()
+    Matrix4x4? oldView = null;
+    Matrix4x4? oldProjection = null;
+
+    public void Render(int width, int height)
     {
+        if (width == -1 || height == -1)
+        {
+            if (Target.IsAvailable)
+            {
+                width = Target.Res!.Width;
+                height = Target.Res!.Height;
+            }
+            else
+            {
+                width = Rlgl.rlGetFramebufferWidth();
+                height = Rlgl.rlGetFramebufferHeight();
+            }
+        }
+
         Rlgl.rlSetBlendMode(BlendMode.BLEND_ADD_COLORS);
         Current = this;
-        Graphics.OldMatView = Graphics.MatView;
-        Graphics.OldMatProjection = Graphics.MatProjection;
+        Graphics.Resolution = new Vector2(width, height);
         Graphics.MatView = Camera.Current.GameObject.View;
-        Graphics.MatProjection = Camera.Current.GetProjectionMatrix(Rlgl.rlGetFramebufferWidth(), Rlgl.rlGetFramebufferHeight());
+        Graphics.MatProjection = Camera.Current.GetProjectionMatrix(width, height);
+        Graphics.OldMatView = oldView ?? Graphics.MatView;
+        Graphics.OldMatProjection = oldProjection ?? Graphics.MatProjection;
 
         Graphics.OldMatViewTransposed = Matrix4x4.Transpose(Graphics.OldMatView);
         Graphics.OldMatProjectionTransposed = Matrix4x4.Transpose(Graphics.OldMatProjection);
@@ -142,6 +160,7 @@ public class Camera : MonoBehaviour
 
         OpaquePass();
 
+#warning TODO: Smarter Shadowmap Updating, updating every frame for every camera is stupid
         Graphics.UpdateAllShadowmaps();
 
         LightingPass();
@@ -154,7 +173,7 @@ public class Camera : MonoBehaviour
         PostProcessStagePostCombine?.Invoke(gBuffer);
 
         // Draw to Screen
-        Target?.Begin();
+        Target.Res?.Begin();
         if (DoClear) Raylib.ClearBackground(ClearColor);
 
         if(debugDraw == DebugDraw.Off)
@@ -172,10 +191,13 @@ public class Camera : MonoBehaviour
         else if (debugDraw == DebugDraw.Velocity)
             DrawFullScreenTexture(gBuffer.Velocity);
 
-        Target?.End();
+        Target.Res?.End();
 
         Current = null;
         Rlgl.rlSetBlendMode(BlendMode.BLEND_ALPHA);
+
+        oldView = Graphics.MatView;
+        oldProjection = Graphics.MatProjection;
     }
 
 }

Prowl.Editor/EditorApplication.cs
@@ -111,8 +111,9 @@ public unsafe class EditorApplication : Application {
 
             OnDrawEditor?.Invoke();
             EditorGui.Update(); 
-            if (Project.HasProject)
-                GameObjectManager.Draw();
+            // Editor doesnt draw the normal way, Rendering is done entirely manually
+            //if (Project.HasProject)
+            //    GameObjectManager.Draw();
             controller.Draw();
 
             Raylib.EndDrawing();

Prowl.Editor/EditorWindows/AssetBrowserWindow.cs
@@ -238,7 +238,8 @@ public class AssetBrowserWindow : EditorWindow {
             var lineColor = AssetsWindow.GetFileColor(file.Extension.ToLower().Trim());
             var pos = ImGui.GetCursorScreenPos();
             drawList.AddLine(new(0, pos.Y), new(pos.X + thumbnailSize, pos.Y + 1f), lineColor, 3f);
-            ImGui.TextUnformatted(Settings.m_HideExtensions ? Path.GetFileNameWithoutExtension(entry.FullName) : Path.GetFileName(entry.FullName));
+            ImGui.TextUnformatted(Settings.m_HideExtensions ? Path.GetFileNameWithoutExtension(entry.FullName) : Path.GetFileName(entry.FullName));        
+            
         }
         else
             ImGui.TextUnformatted(entry.Name);

Prowl.Editor/EditorWindows/ViewportWindow.cs
@@ -7,7 +7,6 @@ using Prowl.Runtime.Components.ImageEffects;
 using Prowl.Runtime.Resources;
 using Prowl.Runtime.SceneManagement;
 using System.Numerics;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace Prowl.Editor.EditorWindows;
 
@@ -70,6 +69,7 @@ public class ViewportWindow : EditorWindow
         // Manually Render to the RenderTexture
         Cam.NearClip = Settings.NearClip;
         Cam.FarClip = Settings.FarClip;
+        Cam.Render((int)windowSize.X, (int)windowSize.Y);
         Settings.RenderResolution = Math.Clamp(Settings.RenderResolution, 0.1f, 8.0f);
         Cam.RenderResolution = Settings.RenderResolution;
 

Prowl.Runtime/GameObjectManager.cs
@@ -130,7 +130,7 @@ public static class GameObjectManager
         Cameras.Sort((a, b) => a.RenderOrder.CompareTo(b.DrawOrder));
         foreach (var cam in Cameras) 
             if(cam.EnabledInHierarchy)
-                cam.Render();
+                cam.Render(-1, -1);
     }
     
 }

Prowl.Runtime/Graphics.cs
@@ -31,6 +31,7 @@ namespace Prowl.Runtime
             Rlgl.rlEnableTexture(texture.id);
         }
 
+        public static Vector2 Resolution;
         public static Matrix4x4 MatView;
         public static Matrix4x4 MatViewTransposed;
         public static Matrix4x4 MatViewInverse;
@@ -70,7 +71,7 @@ namespace Prowl.Runtime
 
             oldTransform ??= transform;
 
-            material.SetVector("Resolution", new Vector2(Rlgl.rlGetFramebufferWidth(), Rlgl.rlGetFramebufferHeight()));
+            material.SetVector("Resolution", Graphics.Resolution);
             //material.SetFloat("Time", (float)Time.time);
             //material.SetFloat("DeltaTime", Time.deltaTimeF);
             //material.SetInt("RandomSeed", Random.Shared.Next());

commit 8cbc7fe45fe3e36b8000fec9ee0e57474a9d4ecf

Fixed Camera.RenderResolution broke from 5e01885

Prowl.Runtime/Components/Camera.cs
@@ -127,19 +127,19 @@ public class Camera : MonoBehaviour
 
     public void Render(int width, int height)
     {
-        if (width == -1 || height == -1)
+        if (Target.IsAvailable)
         {
-            if (Target.IsAvailable)
-            {
-                width = Target.Res!.Width;
-                height = Target.Res!.Height;
-            }
-            else
-            {
-                width = Rlgl.rlGetFramebufferWidth();
-                height = Rlgl.rlGetFramebufferHeight();
-            }
+            width = Target.Res!.Width;
+            height = Target.Res!.Height;
         }
+        else if (width == -1 || height == -1)
+        {
+            width = Rlgl.rlGetFramebufferWidth();
+            height = Rlgl.rlGetFramebufferHeight();
+        }
+
+        width = (int)(width * RenderResolution);
+        height = (int)(height * RenderResolution);
 
         Rlgl.rlSetBlendMode(BlendMode.BLEND_ADD_COLORS);
         Current = this;