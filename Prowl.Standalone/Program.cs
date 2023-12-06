98252d482536733beb6868cac5b099aa7df50eb8
Wulferis <acer1171090@gmail.com>
2023-12-07 01:33:53 +1100

    Added StandaloneAssetProvider
    Standalone should mostly work, just need a Scene Loader.
    Then just need to be able to Build a project for standalone to use

--- Prowl.Standalone/Program.cs
+++ Prowl.Standalone/Program.cs
@@ -3,11 +3,12 @@
 namespace Prowl.Standalone;
 
 internal class Program {
-    
+
     public static int Main(string[] args) {
-        
+
         StandaloneApplication standaloneApplication = new();
         standaloneApplication.Initialize();
+        Application.AssetProvider = new StandaloneAssetProvider();
 #warning TODO: Load Default Scene
         standaloneApplication.Run();
 
---
+++ Prowl.Standalone/StandaloneAssetProvider.cs
@@ -0,0 +1,45 @@
+﻿using Prowl.Runtime;
+using Prowl.Runtime.Utils;
+using System.IO.Compression;
+
+namespace Prowl.Standalone;
+
+public class StandaloneAssetProvider : IAssetProvider
+{
+    public DirectoryInfo Data => new DirectoryInfo(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "GameData"));
+
+    AssetBuildPackage[] packages;
+
+    public StandaloneAssetProvider()
+    {
+        int packageIndex = 0;
+        FileInfo firstPackage = new FileInfo(Path.Combine(Data.FullName, $"Data{packageIndex++}.prowl"));
+        List<AssetBuildPackage> packages = new();
+        while (firstPackage.Exists)
+        {
+            packages.Add(new AssetBuildPackage(firstPackage.OpenRead(), ZipArchiveMode.Read));
+            firstPackage = new FileInfo(Path.Combine(Data.FullName, $"Data{packageIndex++}.prowl"));
+        }
+        this.packages = packages.ToArray();
+    }
+
+    Dictionary<Guid, SerializedAsset> _loaded = [];
+
+    public bool HasAsset(Guid assetID) => _loaded.ContainsKey(assetID);
+
+    public T LoadAsset<T>(string relativeAssetPath) where T : EngineObject
+    {
+        foreach (AssetBuildPackage package in packages)
+            if (package.TryGetAsset(relativeAssetPath, out var asset))
+                return (T)asset.Main;
+        throw new FileNotFoundException($"Asset with path {relativeAssetPath} not found.");
+    }
+
+    public T LoadAsset<T>(Guid guid) where T : EngineObject
+    {
+        foreach (AssetBuildPackage package in packages)
+            if (package.TryGetAsset(guid, out var asset))
+                return (T)asset.Main;
+        throw new FileNotFoundException($"Asset with GUID {guid} not found.");
+    }
+}
