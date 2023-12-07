f839f7657ad4faa3c81493ee32f9fe9598c6080a
Wulferis <acer1171090@gmail.com>
2023-12-07 20:25:01 +1100

    Cleanup Project.cs

--- Prowl.Editor/Projects/Project.cs
+++ Prowl.Editor/Projects/Project.cs
@@ -8,19 +8,16 @@ using System.IO.Compression;
 
 namespace Prowl.Editor;
 
-public static class Project {
-
-    public static event Action OnProjectChanged;
-
-
+/// <summary>
+/// Project is a static class that handles all Project related information and actions
+/// </summary>
+public static class Project
+{
+    #region Public Properties
     public static BuildSettings BuildSettings => Project.ProjectSettings.GetSetting<BuildSettings>();
 
+
     public static string Projects_Directory => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Prowl", "Projects");
-
-    public static ProjectSettings ProjectSettings;
-
-    public static bool HasProject { get; private set; } = false;
-    public static string Name { get; private set; } = "";
     public static string ProjectDirectory => Path.Combine(Projects_Directory, Name);
     public static string ProjectAssetDirectory => Path.Combine(ProjectDirectory, @"Assets");
     public static string ProjectDefaultsDirectory => Path.Combine(ProjectDirectory, @"Defaults");
@@ -33,11 +30,34 @@ public static class Project {
     public static string Editor_Assembly_Proj => Path.Combine(ProjectDirectory, @"CSharp-Editor.csproj");
     public static string Editor_Assembly_DLL => Path.Combine(ProjectDirectory, @"Temp\bin\Debug\net8.0\CSharp-Editor.dll");
 
+
+    public static event Action OnProjectChanged;
+    public static bool HasProject { get; private set; } = false;
+    public static string Name { get; private set; } = "";
+    public static ProjectSettings ProjectSettings;
+    #endregion
+
+    #region Public Methods
+
+    /// <summary>
+    /// Loads a project from a given name
+    /// </summary>
+    /// <param name="projectName">Name of project to load</param>
+    /// <exception cref="UnauthorizedAccessException">Throws if Project Name doesn't exist</exception>
+    /// <exception cref="ArgumentNullException">Throws if projectName is null or empty</exception>
     public static void Open(string projectName) 
     {
         string pathToProject = GetPath(projectName);
-        if (!Directory.Exists(pathToProject)) throw new("Project doesn't exist!");
-        if (string.IsNullOrEmpty(pathToProject)) throw new("Null or Empty Project Path");
+        if (!Directory.Exists(pathToProject))
+        {
+            Runtime.Debug.LogError($"A project with the name {projectName} does not exists at path {pathToProject}");
+            return;
+        }
+        if (string.IsNullOrEmpty(pathToProject))
+        {
+            Runtime.Debug.LogError($"The path to project is Null or Empty.");
+            return;
+        }
 
         AssetDatabase.Clear();
 
@@ -52,19 +72,22 @@ public static class Project {
 
         AssetDatabase.AddRootFolder("Assets");
 
-
         OnProjectChanged?.Invoke();
     }
 
     /// <summary>
     /// Will create a new Project file and its main Folders in a Dedicated folder
     /// </summary>
-    /// <param name="ProjectName"></param>
+    /// <param name="ProjectName">Name of project</param>
     public static void CreateNew(string ProjectName)
     {
         Name = ProjectName;
         string pathToProject = GetPath(ProjectName);
-        if (Directory.Exists(pathToProject)) throw new("Project already exists!");
+        if (Directory.Exists(pathToProject))
+        {
+            Runtime.Debug.LogError($"A project with the name {ProjectName} already exists.");
+            return;
+        }
 
         // Create Path
         Directory.CreateDirectory(pathToProject);
@@ -83,7 +106,153 @@ public static class Project {
         ProjectSettings.Save();
     }
 
-    public static string GetIncludesFrom(IEnumerable<string> filePaths)
+    /// <summary>
+    /// Compiles the game ssembly
+    /// </summary>
+    /// <param name="dir">Directory of the .csproj file to compile</param>
+    /// <param name="isRelease">Is Release Build?</param>
+    /// <returns>True if Compiling was successfull</returns>
+    public static bool Compile(string dir, bool isRelease = false)
+    {
+        if (!HasProject)
+        {
+            Runtime.Debug.LogError($"No Project Loaded...");
+            return false;
+        }
+
+        // Reload CSProject Files
+        BoundedLog($"Starting Project Compilation...");
+        GenerateCSProjectFiles();
+
+        // Compile the Project Assembly using 'dotnet build'
+        BoundedLog($"Compiling external assembly in {dir}...");
+        ProcessStartInfo processInfo = new()
+        {
+            WorkingDirectory = Path.GetDirectoryName(dir),
+            FileName = "cmd.exe",
+            Arguments = $"/c dotnet build \"{Path.GetFileName(dir)}\"" + (isRelease ? " --configuration Release" : ""),
+            CreateNoWindow = true,
+            UseShellExecute = false,
+            RedirectStandardError = true,
+            RedirectStandardOutput = true,
+        };
+        Process process = Process.Start(processInfo) ?? throw new Exception();
+        process.OutputDataReceived += (sender, dataArgs) =>
+        {
+            string? data = dataArgs.Data;
+
+            if (data is null)
+                return;
+
+            if (data.Contains("Warning", StringComparison.OrdinalIgnoreCase))
+                Runtime.Debug.LogWarning(data);
+            else if (data.Contains("Error", StringComparison.OrdinalIgnoreCase))
+                Runtime.Debug.LogError(data);
+            else
+                Runtime.Debug.Log(data);
+        };
+        process.BeginOutputReadLine();
+        process.BeginErrorReadLine();
+        process.ErrorDataReceived += (sender, dataArgs) =>
+        {
+            if (dataArgs.Data is not null)
+                Runtime.Debug.LogError(dataArgs.Data);
+        };
+
+        process.WaitForExit();
+
+        int exitCode = process.ExitCode;
+        process.Close();
+
+        BoundedLog($"Exit Code: '{exitCode}'");
+
+        BoundedLog($"{(exitCode == 0 ? "Successfully" : "Failed to")} compile external assembly!");
+        return exitCode == 0;
+    }
+
+    public static bool BuildProject()
+    {
+        if (!HasProject)
+        {
+            Runtime.Debug.LogError($"No Project Loaded...");
+            return false;
+        }
+
+        if (BuildSettings.StartingScene.IsAvailable == false)
+        {
+            Runtime.Debug.LogError($"No Starting Scene Assigned...");
+            return false;
+        }
+
+        Runtime.Debug.Log($"Starting Project Build...");
+        BoundedLog($"Creating Directories...");
+
+        string BuildPath = Path.Combine(ProjectDirectory, "Builds", "Latest");
+        string BuildDataPath = Path.Combine(ProjectDirectory, "Builds", "Latest", "GameData");
+        // Check if "Latest" folder already exists
+        if (Directory.Exists(BuildPath))
+        {
+            // Increment the folder name
+            int count = 1;
+            string newBuildPath;
+            do
+            {
+                newBuildPath = Path.Combine(ProjectDirectory, "Builds", $"Latest_{count}");
+                count++;
+            } while (Directory.Exists(newBuildPath));
+
+            // Move the existing "Latest" folder to the new one
+            Directory.Move(BuildPath, newBuildPath);
+        }
+
+        Directory.CreateDirectory(BuildPath);
+        Directory.CreateDirectory(BuildDataPath);
+
+
+        BoundedLog($"Compiling project assembly to {BuildPath}...");
+        if (!Compile(Assembly_Proj, true))
+        {
+            Runtime.Debug.LogError($"Failed to compile Project assembly!");
+            return false;
+        }
+
+
+        BoundedLog($"Exporting and Packing assets to {BuildDataPath}...");
+#warning TODO: Needs Asset Dependencies to track what assets are used in built scenes rather then doing all assets
+        AssetDatabase.ExportAllBuildPackages(new DirectoryInfo(BuildDataPath));
+
+
+        BoundedLog($"Preparing default scene to {BuildDataPath}...");
+        FileInfo StartingScene = new FileInfo(Path.Combine(BuildDataPath, "level.prowl"));
+        Tag tag = TagSerializer.Serialize(BuildSettings.StartingScene.Res!);
+        BinaryTagConverter.WriteToFile((CompoundTag)tag, StartingScene);
+
+
+        BoundedLog($"Copying standalone player to {BuildPath}...");
+        // Get the Standalone.zip file from embedded resources
+        using Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"Prowl.Editor.EmbeddedResources.Standalone.zip");
+        using ZipArchive archive = new(stream, ZipArchiveMode.Read);
+        // Extract the Standalone.zip file to the BuildPath
+        archive.ExtractToDirectory(BuildPath);
+
+
+        Runtime.Debug.Log("**********************************************************************************************************************", false);
+        Runtime.Debug.Log($"Successfully built project!");
+
+        // Open the Build folder
+        using Process fileopener = new();
+        fileopener.StartInfo.FileName = "explorer";
+        fileopener.StartInfo.Arguments = "\"" + BuildPath + "\"";
+        fileopener.Start();
+
+        return true;
+    }
+
+    #endregion
+
+
+    #region Private Methods
+    private static string GetIncludesFrom(IEnumerable<string> filePaths)
     {
         List<string> includeElements = new();
         foreach (string filePath in filePaths)
@@ -91,7 +260,7 @@ public static class Project {
         return string.Join("\n", includeElements);
     }
 
-    public static void GenerateCSProjectFiles()
+    private static void GenerateCSProjectFiles()
     {
         if (!HasProject) throw new Exception("No Project Loaded, Cannot generate CS Project Files!");
 
@@ -166,158 +335,17 @@ public static class Project {
         Runtime.Debug.Log("Finished Updating Build Information");
     }
 
-    internal static bool Compile(string dir, bool isRelease = false)
+    private static void BoundedLog(string message)
     {
-        if (!HasProject)
-        {
-            Runtime.Debug.LogError($"No Project Loaded...");
-            return false;
-        }
-
-        Runtime.Debug.Log($"Starting Project Compilation...");
-        Runtime.Debug.Log("**********************************************************************************************************************");
-
-        // Reload CSProject Files
-        GenerateCSProjectFiles();
-
-        Runtime.Debug.Log($"Compiling external assembly in {dir}...");
-        Runtime.Debug.Log("**********************************************************************************************************************");
-
-        ProcessStartInfo processInfo = new()
-        {
-            WorkingDirectory = Path.GetDirectoryName(dir),
-            FileName = "cmd.exe",
-            Arguments = $"/c dotnet build \"{Path.GetFileName(dir)}\"" + (isRelease ? " --configuration Release" : ""),
-            CreateNoWindow = true,
-            UseShellExecute = false,
-            RedirectStandardError = true,
-            RedirectStandardOutput = true,
-        };
-
-        Process process = Process.Start(processInfo) ?? throw new Exception();
-
-        process.OutputDataReceived += (sender, dataArgs) =>
-        {
-            string? data = dataArgs.Data;
-
-            if (data is null)
-                return;
-
-            if (data.Contains("Warning", StringComparison.OrdinalIgnoreCase))
-                Runtime.Debug.LogWarning(data);
-            else if (data.Contains("Error", StringComparison.OrdinalIgnoreCase))
-                Runtime.Debug.LogError(data);
-            else
-                Runtime.Debug.Log(data);
-        };
-        process.BeginOutputReadLine();
-
-        process.ErrorDataReceived += (sender, dataArgs) =>
-        {
-            if (dataArgs.Data is not null)
-                Runtime.Debug.LogError(dataArgs.Data);
-        };
-        process.BeginErrorReadLine();
-
-        process.WaitForExit();
-
-        int exitCode = process.ExitCode;
-        process.Close();
-
-        Runtime.Debug.Log($"Exit Code: '{exitCode}'");
-        Runtime.Debug.Log("**********************************************************************************************************************");
-
-        Runtime.Debug.Log($"{(exitCode == 0 ? "Successfully" : "Failed to")} compile external assembly!");
-        Runtime.Debug.Log("");
-        return true;
+        Runtime.Debug.Log("**********************************************************************************************************************", false);
+        Runtime.Debug.Log(message);
+        Runtime.Debug.Log("**********************************************************************************************************************", false);
     }
 
-    internal static bool BuildProject()
-    {
-        if (!HasProject)
-        {
-            Runtime.Debug.LogError($"No Project Loaded...");
-            return false;
-        }
-
-        if(BuildSettings.StartingScene.IsAvailable == false)
-        {
-            Runtime.Debug.LogError($"No Starting Scene Assigned...");
-            return false;
-        }
-
-        Runtime.Debug.Log($"Starting Project Build...");
-        Runtime.Debug.Log("**********************************************************************************************************************", false);
-        Runtime.Debug.Log($"Creating Directories...");
-        Runtime.Debug.Log("**********************************************************************************************************************", false);
-
-        string BuildPath = Path.Combine(ProjectDirectory, "Builds", "Latest");
-        string BuildDataPath = Path.Combine(ProjectDirectory, "Builds", "Latest", "GameData");
-        // Check if "Latest" folder already exists
-        if (Directory.Exists(BuildPath))
-        {
-            // Increment the folder name
-            int count = 1;
-            string newBuildPath;
-            do
-            {
-                newBuildPath = Path.Combine(ProjectDirectory, "Builds", $"Latest_{count}");
-                count++;
-            } while (Directory.Exists(newBuildPath));
-
-            // Move the existing "Latest" folder to the new one
-            Directory.Move(BuildPath, newBuildPath);
-        }
-
-        Directory.CreateDirectory(BuildPath);
-        Directory.CreateDirectory(BuildDataPath);
-
-
-        Runtime.Debug.Log("**********************************************************************************************************************", false);
-        Runtime.Debug.Log($"Compiling project assembly to {BuildPath}...");
-        Runtime.Debug.Log("**********************************************************************************************************************", false);
-        if (!Compile(Assembly_Proj, true))
-        {
-            Runtime.Debug.LogError($"Failed to compile Project assembly!");
-            return false;
-        }
-
-
-        Runtime.Debug.Log("**********************************************************************************************************************", false);
-        Runtime.Debug.Log($"Exporting and Packing assets to {BuildDataPath}...");
-        Runtime.Debug.Log("**********************************************************************************************************************", false);
-#warning TODO: Needs Asset Dependencies to track what assets are used in built scenes rather then doing all assets
-        AssetDatabase.ExportAllBuildPackages(new DirectoryInfo(BuildDataPath));
-
-
-        Runtime.Debug.Log("**********************************************************************************************************************", false);
-        Runtime.Debug.Log($"Preparing default scene to {BuildDataPath}...");
-        Runtime.Debug.Log("**********************************************************************************************************************", false);
-
-        FileInfo StartingScene = new FileInfo(Path.Combine(BuildDataPath, "level.prowl"));
-        Tag tag = TagSerializer.Serialize(BuildSettings.StartingScene.Res!);
-        BinaryTagConverter.WriteToFile((CompoundTag)tag, StartingScene);
-
-
-        Runtime.Debug.Log("**********************************************************************************************************************", false);
-        Runtime.Debug.Log($"Copying standalone player to {BuildPath}...");
-        Runtime.Debug.Log("**********************************************************************************************************************", false);
-        // Get the Standalone.zip file from embedded resources
-        using Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"Prowl.Editor.EmbeddedResources.Standalone.zip");
-        using ZipArchive archive = new(stream, ZipArchiveMode.Read);
-        // Extract the Standalone.zip file to the BuildPath
-        archive.ExtractToDirectory(BuildPath);
-        
-
-        Runtime.Debug.Log("**********************************************************************************************************************", false);
-
-        Runtime.Debug.Log($"Successfully built project!");
-        Runtime.Debug.Log("");
-        return true;
-    }
-
-    internal static string GetPath(string name)
+    private static string GetPath(string name)
     {
         return Path.Combine(Projects_Directory, name);
     }
+
+    #endregion
 }
