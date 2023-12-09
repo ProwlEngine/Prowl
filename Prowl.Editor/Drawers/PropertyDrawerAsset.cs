da51d801839e847308ddb7589e3c2681a60e994d
Wulferis <acer1171090@gmail.com>
2023-12-09 23:14:56 +1100

    Fixed Asset Property Drawer not drawing colored Rect

--- Prowl.Editor/Drawers/PropertyDrawerAsset.cs
+++ Prowl.Editor/Drawers/PropertyDrawerAsset.cs
@@ -13,31 +13,30 @@ public class PropertyDrawerAsset : PropertyDrawer<IAssetRef>
     protected override bool Draw(string label, ref IAssetRef value, float width)
     {
         bool changed = false;
-        ImDrawListPtr drawList = ImGui.GetWindowDrawList();
         DrawLabel(label, ref width);
 
-        ImGui.PushItemWidth(width);
+        ImGui.SetNextItemWidth(width);
 
         string path;
         if (value.IsExplicitNull)
         {
             path = "(Null)";
-            drawList.AddRectFilled(ImGui.GetItemRectMin(), ImGui.GetItemRectMax(), ImGui.GetColorU32(new System.Numerics.Vector4(0.9f, 0.1f, 0.1f, 0.3f)));
             if (ImGui.Selectable($"{Name}: {path}", false))
             {
                 AssetDatabase.Ping(value.AssetID);
 #warning TODO: Show a popup with a list of all assets of the type - property.Type.Name
             }
+            GUIHelper.ItemRectFilled(0.9f, 0.1f, 0.1f, 0.3f);
         }
         else if (value.IsRuntimeResource)
         {
             path = "(Runtime)" + value.Name;
-            drawList.AddRectFilled(ImGui.GetItemRectMin(), ImGui.GetItemRectMax(), ImGui.GetColorU32(new System.Numerics.Vector4(0.1f, 0.1f, 0.9f, 0.3f)));
             if (ImGui.Selectable($"{Name}: {path}", false))
             {
                 AssetDatabase.Ping(value.AssetID);
 #warning TODO: Show a popup with a list of all assets of the type - property.Type.Name
             }
+            GUIHelper.ItemRectFilled(0.1f, 0.1f, 0.9f, 0.3f);
         }
         else if (AssetDatabase.Contains(value.AssetID))
         {
@@ -65,8 +64,6 @@ public class PropertyDrawerAsset : PropertyDrawer<IAssetRef>
                 Selection.Clear();
             }
         }
-
-        ImGui.PopItemWidth();
         ImGui.Columns(1);
         return changed;
     }
