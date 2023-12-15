cd4b086c2f76533e2ff07d240e9fa1be3a1c1aa0
Wulferis <acer1171090@gmail.com>
2023-12-15 12:49:43 +1100

    Made node editor have Save/Decline buttons instead of auto saving
    Fixes the constant resetting of the editor every time you make a change, was very annoying.

--- Prowl.Editor/EditorWindows/RenderPipelineWindow.cs
+++ Prowl.Editor/EditorWindows/RenderPipelineWindow.cs
@@ -10,6 +10,7 @@ namespace Prowl.Editor.EditorWindows;
 public class RenderPipelineWindow : EditorWindow
 {
     AssetRef<RenderPipeline> CurrentRenderPipeline;
+    bool changed = false;
 
     public RenderPipelineWindow() : base() => Title = FontAwesome6.DiagramProject + " Render Pipeline Editor";
 
@@ -26,16 +27,37 @@ public class RenderPipelineWindow : EditorWindow
 
         if (CurrentRenderPipeline.IsAvailable == false) return;
 
-        bool changed = NodeSystemDrawer.Draw(CurrentRenderPipeline.Res);
+        var size = changed ? new System.Numerics.Vector2(ImGui.GetContentRegionAvail().X, ImGui.GetContentRegionAvail().Y - 30) : ImGui.GetContentRegionAvail();
+        ImGui.BeginChild("RenderPipeline", size, true, ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse);
+        bool changedThisFrame = NodeSystemDrawer.Draw(CurrentRenderPipeline.Res);
+        ImGui.EndChild();
+
+        if(changedThisFrame)
+            CurrentRenderPipeline.Res!.OnValidate();
+
+        changed |= changedThisFrame;
 
         if (changed)
         {
-            // Need to save original asset
-            CurrentRenderPipeline.Res!.OnValidate();
             string relativeAssetPath = AssetDatabase.GUIDToAssetPath(CurrentRenderPipeline.Res!.AssetID);
             var assetFile = AssetDatabase.RelativeToFile(relativeAssetPath);
-            StringTagConverter.WriteToFile((CompoundTag)TagSerializer.Serialize(CurrentRenderPipeline.Res!), assetFile);
-            AssetDatabase.Reimport(AssetDatabase.FileToRelative(assetFile), false);
+
+            // Show Save/Decline buttons in the bottom right corner
+            //ImGui.SetCursorPos(new System.Numerics.Vector2(ImGui.GetStyle().WindowPadding.X + 100, ImGui.GetStyle().WindowPadding.Y + 30));
+            if (ImGui.Button("Save"))
+            {
+                // Need to save original asset
+                StringTagConverter.WriteToFile((CompoundTag)TagSerializer.Serialize(CurrentRenderPipeline.Res!), assetFile);
+                AssetDatabase.Reimport(AssetDatabase.FileToRelative(assetFile), false);
+                changed = false;
+            }
+            ImGui.SameLine();
+            if (ImGui.Button("Decline"))
+            {
+                AssetDatabase.Reimport(AssetDatabase.FileToRelative(assetFile), false);
+                changed = false;
+            }
+
         }
     }
 }
