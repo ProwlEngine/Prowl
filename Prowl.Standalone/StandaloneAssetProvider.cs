d05b6d0a3f517fd84abf6e48b6b8a1deb3b9eaac
Wulferis <acer1171090@gmail.com>
2023-12-18 14:12:32 +1100

    Fixed Asset Load Caching in Standalone Asset Provider

--- Prowl.Standalone/StandaloneAssetProvider.cs
+++ Prowl.Standalone/StandaloneAssetProvider.cs
@@ -13,8 +13,7 @@ public class StandaloneAssetProvider : IAssetProvider
         int packageIndex = 0;
         FileInfo firstPackage = new FileInfo(Path.Combine(Program.Data.FullName, $"Data{packageIndex++}.prowl"));
         List<AssetBuildPackage> packages = new();
-        while (firstPackage.Exists)
-        {
+        while (firstPackage.Exists) {
             packages.Add(new AssetBuildPackage(firstPackage.OpenRead(), ZipArchiveMode.Read));
             firstPackage = new FileInfo(Path.Combine(Program.Data.FullName, $"Data{packageIndex++}.prowl"));
         }
@@ -27,17 +26,44 @@ public class StandaloneAssetProvider : IAssetProvider
 
     public T LoadAsset<T>(string relativeAssetPath) where T : EngineObject
     {
+        Guid guid = GetGuidFromPath(relativeAssetPath);
+        if (_loaded.ContainsKey(guid))
+            return (T)_loaded[guid].Main;
+
         foreach (AssetBuildPackage package in packages)
-            if (package.TryGetAsset(relativeAssetPath, out var asset))
+            if (package.TryGetAsset(relativeAssetPath, out var asset)) {
+                _loaded[guid] = asset;
                 return (T)asset.Main;
+            }
         throw new FileNotFoundException($"Asset with path {relativeAssetPath} not found.");
     }
 
     public T LoadAsset<T>(Guid guid) where T : EngineObject
     {
+        if (_loaded.ContainsKey(guid))
+            return (T)_loaded[guid].Main;
+
         foreach (AssetBuildPackage package in packages)
-            if (package.TryGetAsset(guid, out var asset))
+            if (package.TryGetAsset(guid, out var asset)) {
+                _loaded[guid] = asset;
                 return (T)asset.Main;
+            }
         throw new FileNotFoundException($"Asset with GUID {guid} not found.");
+    }
+
+    public string GetPathFromGUID(Guid guid)
+    {
+        foreach (AssetBuildPackage package in packages)
+            if (package.TryGetPath(guid, out var path))
+                return path;
+        throw new FileNotFoundException($"Asset with GUID {guid} not found.");
+    }
+
+    public Guid GetGuidFromPath(string relativeAssetPath)
+    {
+        foreach (AssetBuildPackage package in packages)
+            if (package.TryGetGuid(relativeAssetPath, out var guid))
+                return guid;
+        throw new FileNotFoundException($"Asset with path {relativeAssetPath} not found.");
     }
 }
