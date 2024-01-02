de028f5fa87781ba558f5268a210cc6c3d697834
Wulferis <acer1171090@gmail.com>
2024-01-03 03:44:37 +1100

    Convex Mesh Collider
    Jitter Physics doesnt appear to support proper Triangle Mesh Colliders yet

--- Prowl.Runtime/Components/Rigidbody.cs
+++ Prowl.Runtime/Components/Rigidbody.cs
@@ -1,11 +1,13 @@
-﻿using Jitter2.Collision.Shapes;
-using Jitter2.Dynamics;
+﻿using Jitter2.Collision;
+using Jitter2.Collision.Shapes;
 using Jitter2.LinearMath;
 using Prowl.Icons;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
+
+
+// Partially based on https://github.com/suzuke/JitterPhysicsForUnity
 
 namespace Prowl.Runtime.Components
 {
@@ -266,5 +268,84 @@ namespace Prowl.Runtime.Components
         public float radius = 1f;
         public float height = 1f;
         public override Shape CreateShape() => new CylinderShape(radius, height);
+    }
+
+    [AddComponentMenu($"{FontAwesome6.HillRockslide}  Physics/{FontAwesome6.Capsules}  Mesh Collider")]
+    public class MeshCollider : Collider
+    {
+        [Header("At the moment Mesh Collider only handle Convex physics! This component is also Experimental!")]
+        public AssetRef<Mesh> mesh;
+
+        public enum Approximation
+        {
+            Level1 = 6,
+            Level2 = 7,
+            Level3 = 8,
+            Level4 = 9,
+            Level5 = 10,
+            Level6 = 11,
+            Level7 = 12,
+            Level8 = 13,
+            Level9 = 15,
+            Level10 = 20,
+            Level15 = 25,
+            Level20 = 30
+        }
+
+        public Approximation convexApprox = Approximation.Level5;
+
+        public override Shape CreateShape()
+        {
+            if (mesh.IsAvailable == false) return new SphereShape(0); // Mesh is missing so we create a sphere with no radius to prevent errors
+
+            var points = mesh.Res.vertices.Select(x => (JVector)x.Position.ToDouble());
+            return new PointCloudShape(BuildConvexCloud(points.ToList()));
+        }
+
+        private List<JVector> BuildConvexCloud(List<JVector> pointCloud)
+        {
+            List<JVector> allIndices = new();
+
+            int steps = (int)convexApprox;
+
+            for (int thetaIndex = 0; thetaIndex < steps; thetaIndex++) {
+                // [0,PI]
+                float theta = MathF.PI / (steps - 1) * thetaIndex;
+                float sinTheta = (float)Math.Sin(theta);
+                float cosTheta = (float)Math.Cos(theta);
+
+                for (int phiIndex = 0; phiIndex < steps; phiIndex++) {
+                    // [-PI,PI]
+                    float phi = (2.0f * MathF.PI) / (steps - 0) * phiIndex - MathF.PI;
+                    float sinPhi = (float)Math.Sin(phi);
+                    float cosPhi = (float)Math.Cos(phi);
+
+                    JVector dir = new JVector(sinTheta * cosPhi, cosTheta, sinTheta * sinPhi);
+
+                    int index = FindExtremePoint(pointCloud, ref dir);
+                    allIndices.Add(pointCloud[index]);
+                }
+            }
+
+            return allIndices.Distinct().ToList();
+        }
+
+        private static int FindExtremePoint(List<JVector> points, ref JVector dir)
+        {
+            int index = 0;
+            float current = float.MinValue;
+
+            JVector point; float value;
+
+            for (int i = 1; i < points.Count; i++) {
+                point = points[i];
+
+                value = JVector.Dot(ref point, ref dir);
+                if (value > current) { current = value; index = i; }
+            }
+
+            return index;
+        }
+
     }
 }
