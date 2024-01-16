64fc2993ccf02bb543d62bf3fbc9a7d6748d2497
Wulferis <acer1171090@gmail.com>
2024-01-17 08:39:52 +1100

    Fixed labels on GameObject's Transform in Inspector

--- Prowl.Editor/EditorWindows/CustomEditors/GameObjectEditor.cs
+++ Prowl.Editor/EditorWindows/CustomEditors/GameObjectEditor.cs
@@ -67,9 +67,9 @@ namespace Prowl.Editor.EditorWindows.CustomEditors
             //ImGui.Separator();
 
 
-            PropertyDrawer.Draw(go.transform, typeof(Transform).GetProperty("position")!);
-            PropertyDrawer.Draw(go.transform, typeof(Transform).GetProperty("eulerAngles")!);
-            PropertyDrawer.Draw(go.transform, typeof(Transform).GetProperty("localScale")!);
+            PropertyDrawer.Draw(go.transform, typeof(Transform).GetProperty("position")!, -1, "Position");
+            PropertyDrawer.Draw(go.transform, typeof(Transform).GetProperty("eulerAngles")!, -1, "Rotation");
+            PropertyDrawer.Draw(go.transform, typeof(Transform).GetProperty("localScale")!, -1, "Scale");
 
             // Draw Components
             HashSet<int> editorsNeeded = new();
