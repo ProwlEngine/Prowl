a0179f8eb4a8d3ebab7c252bed8bd2cb451baad6
Wulferis <acer1171090@gmail.com>
2024-03-21 12:00:00 +1100

    moved CreateDefaults to Project.cs & deleted DefaultAssets.cs

--- Prowl.Editor/Project.cs
+++ Prowl.Editor/Project.cs
@@ -61,7 +61,7 @@ public static class Project
         (EditorApplication.Instance as EditorApplication).RegisterReloadOfExternalAssemblies();
         ProjectSettings = ProjectSettings.Load();
 
-        DefaultAssets.CreateDefaults("Defaults");
+        CreateDefaults("Defaults");
         AssetDatabase.AddRootFolder("Defaults");
         AssetDatabase.ReimportAll();
         AssetDatabase.AddRootFolder("Packages");
@@ -91,7 +91,7 @@ public static class Project
 
         // Create Assets Folder
         Directory.CreateDirectory(Path.Combine(projectDir.FullName, @"Assets"));
-        DefaultAssets.CreateDefaults("Defaults");
+        CreateDefaults("Defaults");
         Directory.CreateDirectory(Path.Combine(projectDir.FullName, @"Library"));
         Directory.CreateDirectory(Path.Combine(projectDir.FullName, @"Packages"));
 
@@ -101,6 +101,31 @@ public static class Project
 
         ProjectSettings = new();
         ProjectSettings.Save();
+    }
+    static void CreateDefaults(string rootFolder)
+    {
+        if (string.IsNullOrWhiteSpace(rootFolder)) throw new ArgumentException("Root Folder cannot be null or whitespace");
+        DirectoryInfo info = new(Path.Combine(Project.ProjectDirectory, rootFolder));
+        if (!info.Exists) info.Create();
+
+#warning TODO: Only copy if the file doesn't exist, or if somehow if the engine version is different or something...
+
+        // Copy embedded defaults to rootFolder, this is just actual Files, so Image.png, not the asset variants
+        foreach (string file in Assembly.GetExecutingAssembly().GetManifestResourceNames().Where(x => x.StartsWith("Prowl.Editor.EmbeddedResources.DefaultAssets.")))
+        {
+            string[] nodes = file.Split('.');
+            string fileName = nodes[^2];
+            string fileExtension = nodes[^1];
+            string filePath = Path.Combine(info.FullName, fileName + "." + fileExtension);
+            if (File.Exists(filePath))
+                File.Delete(filePath);
+            //if (!File.Exists(filePath))
+            {
+                using Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(file);
+                using FileStream fileStream = File.Create(filePath);
+                stream.CopyTo(fileStream);
+            }
+        }
     }
 
     /// <summary>
