948f645228d78255f63f2f3106d050d81b190a23
Wulferis <acer1171090@gmail.com>
2024-03-28 14:09:39 +1100

    Partially implemented Animation system similiar to Unity's old Legacy animation
    Decided to go a different route for animation, something simpler, but gonna leave this as a Branch incase anyone wants to have a go at finishing this implementation.

---
+++ Prowl.Runtime/Components/Animation.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using static Prowl.Runtime.AnimationClip;
+
+namespace Prowl.Runtime
+{
+    public class Animation : MonoBehaviour
+    {
+
+        public List<AssetRef<AnimationClip>> Clips;
+        public AssetRef<AnimationClip> DefaultClip;
+        public bool PlayAutomatically = true;
+        public double Speed = 1.0;
+
+        private Dictionary<string, ActiveClip> activeClips = new(StringComparer.OrdinalIgnoreCase);
+
+        public override void OnEnable()
+        {
+            // Assign DefaultClip to the first clip if it's not set
+            if (!DefaultClip.IsAvailable && Clips.Count > 0)
+                DefaultClip = Clips[0];
+
+            foreach (var clip in Clips)
+                if(clip.IsAvailable)
+                    ProcessClip(clip.Res!);
+            if (DefaultClip.IsAvailable) {
+                ProcessClip(DefaultClip.Res!);
+                if (PlayAutomatically)
+                    Play(DefaultClip.Res!.Name);
+            }
+        }
+
+        public void AddClip(AnimationClip clip) => ProcessClip(clip);
+
+        public void Blend(string clipName, double targetWeight, double fadeLength = 0.3f)
+        {
+            if (activeClips.ContainsKey(clipName))
+                activeClips[clipName].CrossFade(targetWeight, fadeLength);
+        }
+
+        public void CrossFade(string clipName, double fadeLength = 0.3f)
+        {
+            double totalWeight = 0;
+            foreach (var activeClip in activeClips)
+            {
+                if (activeClip.Key.Equals(clipName, StringComparison.OrdinalIgnoreCase))
+                    activeClip.Value.CrossFade(1, fadeLength);
+                else
+                    activeClip.Value.CrossFade(0, fadeLength);
+
+                totalWeight += activeClip.Value.Weight;
+            }
+
+            // Normalize weights
+            foreach (var activeClip in activeClips)
+                activeClip.Value.NormalizeWeight(totalWeight);
+        }
+
+        public ActiveClip? Play(string clipName) 
+        {
+            if (activeClips.TryGetValue(clipName, out ActiveClip? value))
+            {
+                value.Play();
+                return value;
+            }
+            return null;
+        }
+
+        public void Stop() 
+        { 
+            // Stop all
+            foreach (var activeClip in activeClips)
+                activeClip.Value.Stop();
+        }
+
+        public override void Update()
+        {
+
+
+            foreach (var activeClip in activeClips)
+                activeClip.Value.Update(this);
+        }
+
+
+        private void ProcessClip(AnimationClip clip)
+        {
+            foreach (var nodeName in clip.Targets.Keys)
+            {
+                var target = this.GameObject.transform.DeepFind(nodeName);
+                if (target == null)
+                    continue;
+
+                ActiveClip activeClip = new(clip);
+
+                var tracks = clip.Targets[nodeName];
+                foreach (var track in tracks)
+                {
+                    // Transform is a special case
+                    if (track.Component == typeof(Transform))
+                    {
+                        activeClip.Tracks.Add(new ActiveTracks(target, track));
+                        continue;
+                    }
+
+                    // Find the component on the target
+                    var component = target.gameObject.GetComponent(track.Component);
+                    if (component == null)
+                        continue;
+
+                    activeClip.Tracks.Add(new ActiveTracks(component, track));
+                }
+            
+            }
+        }
+
+        public class ActiveClip(AnimationClip clip)
+        {
+            public AnimationClip Clip = clip;
+
+            public bool Playing = false;
+            public double Time = 0;
+            public List<ActiveTracks> Tracks = [];
+
+            public double Weight => weight;
+
+            private double weight = 0;
+
+            private double targetWeight = 0;
+            private double fadeLength = 0.3f;
+            private double startTime = 0;
+
+            private double direction = 1.0f;
+
+            public void NormalizeWeight(double totalWeight)
+            {
+                weight /= totalWeight;
+            }
+
+            public void CrossFade(double targetWeight, double fadeLength)
+            {
+                this.targetWeight = targetWeight;
+                this.fadeLength = fadeLength;
+                startTime = Runtime.Time.time;
+            }
+
+            public void Play() => Playing = true;
+
+            public void Stop()
+            {
+                Playing = false;
+                Time = 0;
+                weight = 0;
+            }
+
+            public void Update(Animation host)
+            {
+                if (!Playing)
+                    return;
+
+                Time += (Runtime.Time.deltaTimeF * host.Speed) * direction;
+                if (Time > Clip.Duration)
+                {
+                    if (Clip.Wrap == WrapMode.Once)
+                    {
+                        Playing = false;
+                        Time = 0;
+                        weight = 0;
+                    }
+                    else if (Clip.Wrap == WrapMode.Loop)
+                        Time = 0;
+                    else if (Clip.Wrap == WrapMode.PingPong)
+                    {
+                        Time = Clip.Duration;
+                        direction *= -1; // Reverse the direction
+                    }
+                    else if (Clip.Wrap == WrapMode.ClampForever)
+                    {
+                        Time = Clip.Duration;
+                    }
+                }
+
+                // Update Weight
+                if (weight != targetWeight)
+                {
+                    var t = (Runtime.Time.time - startTime) / fadeLength;
+                    weight = Mathf.LerpClamped(weight, targetWeight, t);
+                }
+            }
+        }
+
+        public class ActiveTracks
+        {
+            public object Target;
+
+            public double value = 0;
+            private Track track;
+
+            public ActiveTracks(object target, Track animCurve)
+            {
+                Target = target;
+                track = animCurve;
+
+                value = track.Curve.Evaluate(0);
+            }
+
+            public void SetField(double value, double weight)
+            {
+                // Weights should always be normalized between all active clips
+                // So it should be safe to apply the weight here
+                if (track.Parent == null)
+                {
+                    track.Member.SetValue(Target, value * weight);
+                }
+                else
+                {
+                    var parent = track.Parent.GetValue(Target);
+                    track.Member.SetValue(parent, value * weight);
+                }
+            }
+        }
+
+    }
+}
--- Prowl.Runtime/Resources/AnimationClip.cs
+++ Prowl.Runtime/Resources/AnimationClip.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
 namespace Prowl.Runtime
 {
@@ -20,17 +21,57 @@ namespace Prowl.Runtime
 
         public WrapMode Wrap;
 
-        public MultiValueDictionary<string, AnimCurve> Curves { get; private set; } = [];
+        public MultiValueDictionary<string, Track> Targets { get; private set; } = [];
 
-        public void ClearCurves() => Curves.Clear();
+        public void ClearCurves() => Targets.Clear();
 
         public void SetCurve(string nodeName, Type component, string propertyName, AnimationCurve curve)
         {
-            Curves.Add(nodeName, new AnimCurve {
+            var animCurve = new Track {
                 Component = component,
                 PropertyName = propertyName,
                 Curve = curve
-            });
+            };
+
+            // Cache MemberInfo
+            // PropertyName can be a field or property name
+            // And can either be the name directly or a path to the property
+            // e.g. "playerSpeed" or "localPosition.x"
+            // It can however only be a simple path, no arrays or lists and only 1 level deep
+            // As well as only floats and doublesCertainly! Here's an updated implementation of the AnimationClip class with support for caching the MemberInfo and handling property names that can go one level deep:
+
+            var propertyParts = propertyName.Split('.');
+            if (propertyParts.Length > 2)
+                throw new Exception($"PropertyName {propertyName} is too complex. Only simple paths (1 level deep) are supported.");
+
+            MemberInfo member;
+            MemberInfo parent = null;
+            if (propertyParts.Length == 1)
+            {
+                animCurve.isDirect = true;
+                member = component.GetMember(propertyName)?.FirstOrDefault() ?? throw new Exception($"Member {propertyName} not found on {component.FullName}");
+            }
+            else
+            {
+                animCurve.isDirect = false;
+                parent = component.GetMember(propertyParts[0])?.FirstOrDefault() ?? throw new Exception($"Member {propertyParts[0]} not found on {component.FullName}");
+                if (parent == null)
+                    throw new Exception($"Member {propertyParts[0]} not found on {component.FullName}");
+
+                var parentType = (parent as PropertyInfo)?.PropertyType ?? (parent as FieldInfo)?.FieldType;
+                member = parentType.GetMember(propertyParts[1])?.FirstOrDefault() ?? throw new Exception($"Member {propertyParts[1]} not found on {parentType.FullName}");
+            }
+
+            var memberType = (member as PropertyInfo)?.PropertyType ?? (member as FieldInfo)?.FieldType;
+            if (memberType != typeof(double))
+                throw new Exception($"Member {propertyName} on {component.FullName} is not of type double.");
+
+            animCurve.Parent = parent;
+            animCurve.Member = member;
+
+
+
+            Targets.Add(nodeName, animCurve);
         }
 
         public void Deserialize(SerializedProperty value, Serializer.SerializationContext ctx)
@@ -40,16 +81,16 @@ namespace Prowl.Runtime
             DurationInTicks = value.Get("DurationInTicks").DoubleValue;
             Wrap = (WrapMode)value.Get("Wrap").IntValue;
 
-            var curveList = value.Get("Curves");
-            foreach (var curve in curveList.List)
+            var targets = value.Get("Targets");
+            foreach (var target in targets.List)
             {
-                var nodeName = curve.Get("NodeName").StringValue;
-                var animCurves = curve.Get("AnimCurves");
-                foreach (var animCurveProp in animCurves.List)
+                var nodeName = target.Get("NodeName").StringValue;
+                var tracks = target.Get("Tracks");
+                foreach (var trackProp in tracks.List)
                 {
-                    var component = RuntimeUtils.FindType(animCurveProp.Get("Component").StringValue);
-                    var propertyName = animCurveProp.Get("PropertyName").StringValue;
-                    var curveValue = Serializer.Deserialize<AnimationCurve>(animCurveProp.Get("Curve"), ctx);
+                    var component = RuntimeUtils.FindType(trackProp.Get("Component").StringValue);
+                    var propertyName = trackProp.Get("PropertyName").StringValue;
+                    var curveValue = Serializer.Deserialize<AnimationCurve>(trackProp.Get("Curve"), ctx);
                     SetCurve(nodeName, component, propertyName, curveValue);
                 }
 
@@ -64,35 +105,39 @@ namespace Prowl.Runtime
             value.Add("DurationInTicks", new SerializedProperty(DurationInTicks));
             value.Add("Wrap", new SerializedProperty((int)Wrap));
 
-            var curveList = SerializedProperty.NewList();
-            foreach (var curve in Curves)
+            var targets = SerializedProperty.NewList();
+            foreach (var kvp in Targets)
             {
-                var curveValue = SerializedProperty.NewCompound();
-                curveValue.Add("NodeName", new SerializedProperty(curve.Key));
-                var animCurves = SerializedProperty.NewList();
-                foreach (var animCurve in curve.Value)
+                var kvpProp = SerializedProperty.NewCompound();
+                kvpProp.Add("NodeName", new SerializedProperty(kvp.Key));
+                var tracks = SerializedProperty.NewList();
+                foreach (var track in kvp.Value)
                 {
-                    var animCurveProp = SerializedProperty.NewCompound();
-                    animCurveProp.Add("Component", new SerializedProperty(animCurve.Component.FullName));
-                    animCurveProp.Add("PropertyName", new SerializedProperty(animCurve.PropertyName));
-                    animCurveProp.Add("Curve", Serializer.Serialize(animCurve.Curve, ctx));
-                    animCurves.ListAdd(animCurveProp);
+                    var trackProp = SerializedProperty.NewCompound();
+                    trackProp.Add("Component", new SerializedProperty(track.Component.FullName));
+                    trackProp.Add("PropertyName", new SerializedProperty(track.PropertyName));
+                    trackProp.Add("Curve", Serializer.Serialize(track.Curve, ctx));
+                    tracks.ListAdd(trackProp);
                 }
-                curveValue.Add("AnimCurves", animCurves);
+                kvpProp.Add("Tracks", tracks);
 
-                curveList.ListAdd(curveValue);
+                targets.ListAdd(kvpProp);
             }
 
-            value.Add("Curves", curveList);
+            value.Add("Targets", targets);
             return value;
         }
 
 
-        public class AnimCurve
+        public class Track
         {
             public Type Component;
             public string PropertyName;
             public AnimationCurve Curve;
+
+            public bool isDirect;
+            public MemberInfo Parent;
+            public MemberInfo Member;
         }
 
     }
