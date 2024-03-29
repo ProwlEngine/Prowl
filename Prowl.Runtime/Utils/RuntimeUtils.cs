0fdfbe813ac05ffe3a854ff9be17b9ac8d28b4ad
Wulferis <acer1171090@gmail.com>
2024-03-29 11:29:10 +1100

    Added Runtime.IsWindows(), IsLinux(), IsMax(), IsFreeBSD()

--- Prowl.Runtime/Utils/RuntimeUtils.cs
+++ Prowl.Runtime/Utils/RuntimeUtils.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Silk.NET.Vulkan;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +11,11 @@ namespace Prowl.Runtime
 {
     public static class RuntimeUtils
     {
+
+        public static bool IsWindows() => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+        public static bool IsLinux() => RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+        public static bool IsFreeBSD() => RuntimeInformation.IsOSPlatform(OSPlatform.FreeBSD);
+        public static bool IsMac() => RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
 
         public static Type FindType(string qualifiedTypeName)
         {
