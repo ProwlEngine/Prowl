7ee2ae21ce2af9ec9bba6eee6bea006387ee7a26
Wulferis <acer1171090@gmail.com>
2024-03-30 01:12:52 +1100

    Added ScriptableSingleton from Unity - For future Editor Settings

---
+++ Prowl.Editor/Utilities/ScriptableSingleton.cs
@@ -0,0 +1,84 @@
+﻿using Prowl.Runtime;
+
+namespace Prowl.Editor.Utilities
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class FilePathAttribute : Attribute
+    {
+        public enum Location
+        {
+            ProjectFolder,
+            ProjectSettingsFolder,
+            PreferencesFolder
+        }
+
+        public string FilePath { get; }
+        public Location FileLocation { get; }
+
+        public FilePathAttribute(string filePath, Location fileLocation)
+        {
+            FilePath = filePath;
+            FileLocation = fileLocation;
+        }
+    }
+
+    public abstract class ScriptableSingleton<T> where T : ScriptableSingleton<T>, new()
+    {
+        private static T instance;
+
+        public static T Instance {
+            get {
+                if (instance == null)
+                {
+                    instance = LoadOrCreateInstance();
+                }
+                return instance;
+            }
+        }
+
+        protected void Save()
+        {
+            StringTagConverter.WriteToFile(Serializer.Serialize(this), new(GetFilePath()));
+        }
+
+        protected string GetFilePath()
+        {
+            var attribute = Attribute.GetCustomAttribute(GetType(), typeof(FilePathAttribute)) as FilePathAttribute;
+            if (attribute != null)
+            {
+                string directory = string.Empty;
+                switch (attribute.FileLocation)
+                {
+                    case FilePathAttribute.Location.ProjectFolder:
+                        directory = Project.ProjectDirectory;
+                        break;
+                    case FilePathAttribute.Location.ProjectSettingsFolder:
+                        directory = Path.Combine(Project.ProjectDirectory, "ProjectSettings");
+                        break;
+                    case FilePathAttribute.Location.PreferencesFolder:
+                        // Persistent across all projects
+                        directory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Prowl", "Editor");
+                        break;
+                }
+                return Path.Combine(directory, attribute.FilePath);
+            }
+            return string.Empty;
+        }
+
+        private static T LoadOrCreateInstance()
+        {
+            string filePath = new T().GetFilePath();
+
+            if (File.Exists(filePath))
+            {
+                return Serializer.Deserialize<T>(StringTagConverter.ReadFromFile(new FileInfo(filePath)));
+            }
+            else
+            {
+                var newInstance = new T();
+                newInstance.Save();
+                return newInstance;
+            }
+        }
+    }
+}
