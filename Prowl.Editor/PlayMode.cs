c4dc35379ccd9cbb1f52b9a3a93f5aa29a2cbbc1
Wulferis <acer1171090@gmail.com>
2024-03-29 19:25:04 +1100

    PlayMode Clear Before disabling scripts so OnDisable/OnDestroy get called

--- Prowl.Editor/PlayMode.cs
+++ Prowl.Editor/PlayMode.cs
@@ -45,6 +45,7 @@ public static class PlayMode {
     
     public static void Stop() {
         Current = Mode.Editing;
+        SceneManager.Clear();
         MonoBehaviour.PauseLogic = true;
 
         SceneManager.RestoreScene();
