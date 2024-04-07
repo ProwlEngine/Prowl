66952c11e7dc49f5d1352e00baeb36e3bbaf50e8
Wulferis <acer1171090@gmail.com>
2024-04-07 21:21:34 +1000

    ScriptedImported dont need to be EngineObjects

--- Prowl.Editor/Assets/ScriptedImporter.cs
+++ Prowl.Editor/Assets/ScriptedImporter.cs
@@ -3,7 +3,7 @@ using Prowl.Runtime.Utils;
 
 namespace Prowl.Editor.Assets
 {
-    public class ScriptedImporter : EngineObject
+    public class ScriptedImporter
     {
         public virtual void Import(SerializedAsset ctx, FileInfo assetPath) { }
     }
