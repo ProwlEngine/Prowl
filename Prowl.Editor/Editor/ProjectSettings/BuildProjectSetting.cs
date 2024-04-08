6c6dba1df9d28abdbf37162444a329090a070d52
Wulferis <acer1171090@gmail.com>
2024-04-08 13:19:44 +1000

    Made BuildSettings a Editor instead of Project setting

--- Prowl.Editor/Editor/ProjectSettings/BuildProjectSetting.cs
+++ Prowl.Editor/Editor/ProjectSettings/BuildProjectSetting.cs
@@ -1,10 +1,9 @@
-﻿using Prowl.Editor.Utilities;
-using Prowl.Runtime;
+﻿using Prowl.Runtime;
 using Prowl.Runtime.Utils;
 
 namespace Prowl.Editor.Editor.ProjectSettings
 {
-    [FilePath("BuildSettings.projsetting", FilePathAttribute.Location.Setting)]
+    [FilePath("BuildSettings.projsetting", FilePathAttribute.Location.EditorSetting)]
     public class BuildProjectSetting : ScriptableSingleton<BuildProjectSetting>
     {
         public AssetRef<Scene> InitialScene;
