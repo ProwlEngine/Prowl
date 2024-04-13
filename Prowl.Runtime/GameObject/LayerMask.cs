fba9e7c4b9d65363c9b7ebc84bbe502886d5bf2e
Wulferis <acer1171090@gmail.com>
2024-04-14 09:21:12 +1000

    Improved LayerMask class

--- Prowl.Runtime/GameObject/LayerMask.cs
+++ Prowl.Runtime/GameObject/LayerMask.cs
@@ -2,53 +2,22 @@
 
 public class LayerMask
 {
-    private int mask;
+    private uint mask = 0;
 
-    public LayerMask()
+    public bool Intersects(LayerMask other) => HasLayer(other.mask);
+    public bool HasLayer(uint index) => (mask & (1 << (int)index)) == (1 << (int)index);
+    public void SetLayer(uint index) => mask |= 1u << (int)index;
+    public void RemoveLayer(uint index) => mask &= ~(1u << (int)index);
+    public static LayerMask operator |(LayerMask mask1, LayerMask mask2) => new() { mask = mask1.mask | mask2.mask };
+    public static LayerMask operator &(LayerMask mask1, LayerMask mask2) => new() { mask = mask1.mask & mask2.mask };
+    public override bool Equals(object obj)
     {
-        mask = 0;
+        if (obj is null || !(obj is LayerMask other))
+            return false;
+        return mask == other.mask;
     }
+    public override int GetHashCode() => mask.GetHashCode();
 
-    public void AddLayer(string layer)
-    {
-        int layerIndex = TagLayerManager.GetLayerIndex(layer);
-        mask |= 1 << layerIndex;
-    }
-
-    public void RemoveLayer(string layer)
-    {
-        int layerIndex = TagLayerManager.GetLayerIndex(layer);
-        if (layerIndex != -1)
-            mask &= ~(1 << layerIndex);
-    }
-
-    public bool Contains(string layer)
-    {
-        int layerIndex = TagLayerManager.GetLayerIndex(layer);
-        return layerIndex != -1 && ((mask >> layerIndex) & 1) != 0;
-    }
-
-    public bool Intersects(LayerMask other)
-    {
-        return (mask & other.mask) != 0;
-    }
-
-    public static implicit operator int(LayerMask mask)
-    {
-        return mask.mask;
-    }
-
-    public static LayerMask operator |(LayerMask mask1, LayerMask mask2)
-    {
-        LayerMask result = new LayerMask();
-        result.mask = mask1.mask | mask2.mask;
-        return result;
-    }
-
-    public static LayerMask operator &(LayerMask mask1, LayerMask mask2)
-    {
-        LayerMask result = new LayerMask();
-        result.mask = mask1.mask & mask2.mask;
-        return result;
-    }
+    public static string LayerToName(int index) => TagLayerManager.GetLayer(index);
+    public static int NameToLayer(string name) => TagLayerManager.GetLayerIndex(name);
 }
