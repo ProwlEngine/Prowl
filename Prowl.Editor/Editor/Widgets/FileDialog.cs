b4f2002218e0f847b72928f32c33564b242c4330
Abdiel Lopez <a.paperprototype@gmail.com>
2024-06-09 08:27:05 -0600

    File Dialog works for creating files

--- Prowl.Editor/Editor/Widgets/FileDialog.cs
+++ Prowl.Editor/Editor/Widgets/FileDialog.cs
@@ -1,4 +1,8 @@
-﻿namespace Prowl.Editor
+﻿using Prowl.Runtime.GUI;
+using Prowl.Runtime;
+using Prowl.Icons;
+
+namespace Prowl.Editor
 {
 
     // 1. rename the configs to be easier to understand
@@ -33,8 +37,8 @@
         private FileDialogSortBy sortByPrevious = FileDialogSortBy.None;
         private bool sortDown = false;
 
-        private Stack<object> _BackStack = new();
-        private Stack<object> _ForwardStack = new();
+        private Stack<DirectoryInfo> _BackStack = new();
+        private Stack<DirectoryInfo> _ForwardStack = new();
 
         protected override bool Center { get; } = true;
         protected override double Width { get; } = 740;
@@ -57,52 +61,160 @@
 
         protected override void Draw()
         {
-            //bool complete = false;
-            //
-            //g.CurrentNode.Layout(LayoutType.Column);
-            //g.CurrentNode.ScaleChildren();
-            //g.CurrentNode.Padding(10, 10, 10, 10);
-            //
-            //using (g.Node("Header").ExpandWidth().MaxHeight(GuiStyle.ItemHeight).Layout(LayoutType.Row).ScaleChildren().Enter())
-            //{
-            //    var textSize = UIDrawList.DefaultFont.CalcTextSize(Dialog.title, 0);
-            //    using (g.Node("Title").Expand().MaxWidth(textSize.x).Enter())
-            //    {
-            //        g.DrawText(UIDrawList.DefaultFont, Dialog.title, 20, g.CurrentNode.LayoutData.InnerRect, GuiStyle.Base10);
-            //    }
-            //
-            //    using (g.ButtonNode("BackBtn", out var pressed, out var hovered).Expand().MaxWidth(GuiStyle.ItemHeight).Enter())
-            //    {
-            //        g.DrawText(UIDrawList.DefaultFont, FontAwesome6.ChevronLeft, 20, g.CurrentNode.LayoutData.InnerRect, hovered ? GuiStyle.Base11 : GuiStyle.Base4);
-            //    }
-            //
-            //    using (g.ButtonNode("ForwardBtn", out var pressed, out var hovered).Expand().MaxWidth(GuiStyle.ItemHeight).Enter())
-            //    {
-            //        g.DrawText(UIDrawList.DefaultFont, FontAwesome6.ChevronRight, 20, g.CurrentNode.LayoutData.InnerRect, hovered ? GuiStyle.Base11 : GuiStyle.Base4);
-            //    }
-            //
-            //    using (g.Node("Search").Expand().Enter())
-            //    {
-            //        g.Search("SearchInput", ref _searchText, 0, 0, Size.Percentage(1f), GuiStyle.ItemHeight);
-            //    }
-            //    using (g.Node("Search").Expand().Enter())
-            //    {
-            //        g.Search("SearchInput", ref _searchText, 0, 0, Size.Percentage(1f), GuiStyle.ItemHeight);
-            //    }
-            //
-            //
-            //    g.Search("SearchInput", ref _searchText, 0, 0, Size.Percentage(1f), GuiStyle.ItemHeight);
-            //}
-            //
-            //// Clicked outside Window
-            //if (g.IsPointerClick(Silk.NET.Input.MouseButton.Left) && !g.IsHovering())
-            //{
-            //    isOpened = false;
-            //    Dialog.OnCancel?.Invoke();
-            //}
-            //
-            //if (complete)
-            //    isOpened = false;
+            bool complete = false;
+
+            gui.CurrentNode.Layout(LayoutType.Column);
+            gui.CurrentNode.ScaleChildren();
+            gui.CurrentNode.Padding(10, 10, 10, 10);
+
+            using (gui.Node("Header").ExpandWidth().MaxHeight(GuiStyle.ItemHeight).Layout(LayoutType.Row).ScaleChildren().Enter())
+            {
+                using (gui.Node("Title").Expand().Enter())
+                {
+                    gui.Draw2D.DrawText(Dialog.title, 20, gui.CurrentNode.LayoutData.InnerRect, GuiStyle.Base10);
+                }
+
+                using (gui.Node("BackBtn").Expand().Enter())
+                {
+                    var hovered = gui.IsNodeHovered() || _BackStack.Count > 0;
+                    gui.Draw2D.DrawText(FontAwesome6.ChevronLeft, 20, gui.CurrentNode.LayoutData.InnerRect, hovered ? GuiStyle.Base11 : GuiStyle.Base4);
+                    
+                    if (gui.IsNodePressed() && _BackStack.Count > 0)
+                    {
+                        Debug.Log("popping top item");
+                        Dialog.directoryPath = _BackStack.Pop();
+                    }
+                }
+
+                using (gui.Node("ForwardBtn").Expand().Enter())
+                {
+                    var hovered = gui.IsNodeHovered();
+                    gui.Draw2D.DrawText(FontAwesome6.ChevronRight, 20, gui.CurrentNode.LayoutData.InnerRect, hovered ? GuiStyle.Base11 : GuiStyle.Base4);
+                }
+
+                using (gui.Node("FileName").Expand().Enter())
+                {
+                    gui.Search("FileName", ref Dialog.fileName, 0, 0, Size.Percentage(1f), GuiStyle.ItemHeight);
+                }
+
+                using (gui.Node("SaveBtn").Expand().Enter())
+                {
+                    var hovered = gui.IsNodeHovered();
+                    gui.Draw2D.DrawText("Save", 20, gui.CurrentNode.LayoutData.InnerRect, hovered ? GuiStyle.Base11 : GuiStyle.Base4);
+                    if (gui.IsNodePressed())
+                    {
+                        var path = Path.Combine(Dialog.directoryPath.FullName + "/" + Dialog.fileName);
+                        Debug.Log("saving as" + path);
+                        Dialog.OnComplete?.Invoke(path);
+                        isOpened = false;
+                    }
+                }
+
+                if (complete)
+                {
+                    isOpened = false;
+                }
+            }
+            
+            using (gui.Node("Content").ExpandWidth().Layout(LayoutType.Column).Enter())
+            {
+                DirectoryInfo[] directories = Dialog.directoryPath.GetDirectories();
+                int index = 0;
+                foreach (var directory in directories)
+                {
+                    using (gui.Node("Content" + index).ExpandWidth().Height(GuiStyle.ItemHeight).ScaleChildren()
+                               .Layout(LayoutType.Row).Enter())
+                    {
+                        if (gui.IsNodePressed())
+                        {
+                            _BackStack.Push(directory);
+                            Dialog.directoryPath = directory;
+                        }
+                        
+                        var hovered = gui.IsNodeHovered();
+                        Color bg = hovered ? GuiStyle.Base11 : GuiStyle.Base4;
+                        using (gui.Node("FileName").Expand().Enter())
+                        {
+                            gui.Draw2D.DrawText(FontAwesome6.Folder + directory.Name, 20, gui.CurrentNode.LayoutData.InnerRect.Min,bg);
+                        }
+                        
+                        using (gui.Node("FileDate").Expand().Enter())
+                        {
+                            gui.Draw2D.DrawText(directory.LastWriteTime.ToString(), 20, gui.CurrentNode.LayoutData.InnerRect.Min,bg);
+                        }
+                        
+                        using (gui.Node("SaveBtn").Expand().Enter())
+                        {
+                            gui.Draw2D.DrawText("Save", 20, gui.CurrentNode.LayoutData.InnerRect,bg);
+                        }
+
+                        index++;
+                    }
+                }
+                
+                FileInfo[] files = Dialog.directoryPath.GetFiles();
+                foreach (var file in files)
+                {
+                    using (gui.Node("Content" + index).ExpandWidth().Height(GuiStyle.ItemHeight).ScaleChildren()
+                               .Layout(LayoutType.Row).Enter())
+                    {
+                        var hovered = gui.IsNodeHovered();
+                        Color bg = hovered ? GuiStyle.Base11 : GuiStyle.Base4;
+                        using (gui.Node("FileName").Expand().Enter())
+                        {
+                            gui.Draw2D.DrawText(file.Name, 20, gui.CurrentNode.LayoutData.InnerRect.Min, bg);
+                        }
+                        
+                        using (gui.Node("FileDate").Expand().Enter())
+                        {
+                            gui.Draw2D.DrawText(file.LastWriteTime.ToString(), 20, gui.CurrentNode.LayoutData.InnerRect.Min, bg);
+                        }
+
+                        using (gui.Node("FileDate").Expand().Enter())
+                        {
+                            gui.Draw2D.DrawText(toMemSizeReadable(file.Length), 20, gui.CurrentNode.LayoutData.InnerRect.Min, bg);
+                        }
+                        
+                        using (gui.Node("SaveBtn").Expand().Enter())
+                        {
+                            gui.Draw2D.DrawText("Save", 20, gui.CurrentNode.LayoutData.InnerRect, bg);
+                        }
+
+                        index++;
+                    }
+                }
+            }
+            
+            // Clicked outside Window
+            if (gui.IsPointerClick(Silk.NET.Input.MouseButton.Left) && !gui.IsPointerHovering())
+            {
+                Debug.Log("clicked outside of window");
+                isOpened = false;
+                Dialog.OnCancel?.Invoke();
+            }
+        }
+
+        private string toMemSizeReadable(long length)
+        {
+            if (length < 1000)
+            {
+                // bytes
+                return length + "bytes";
+            }
+            // if smaller than megabyte => kilobyte
+            if (length < 1000000)
+            {
+                // kilobyte
+                return length / 1000f + "kb";
+            }
+            if (length < 1000000000)
+            {
+                // megabyte
+                return length / 1000000f + "mb";
+            }
+            
+            // gigabyte
+            return length / 1000000000f + "gb";
         }
 
         private void Sort(bool forceSort = false)
