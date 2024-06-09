d4ec178563af91ffe1db45837f3f5d88ab3fadec
Kai Angulo <kangriebling@gmail.com>
2024-06-08 21:34:29 -0700

    Edited name definition in AssetDatabase.Core

--- Prowl.Editor/Assets/AssetDatabase.Core.cs
+++ Prowl.Editor/Assets/AssetDatabase.Core.cs
@@ -42,13 +42,13 @@ namespace Prowl.Editor.Assets
 
         internal static void InternalUpdate()
         {
-            if (Window.IsFocused)
+            if (Screen.IsFocused)
             {
                 RefreshTimer += Time.deltaTime;
                 if (!lastFocused || RefreshTimer > 5f)
                     Update();
             }
-            lastFocused = Window.IsFocused;
+            lastFocused = Screen.IsFocused;
         }
 
         /// <summary>
