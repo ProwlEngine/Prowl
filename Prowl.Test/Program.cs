2ae7de5fd89495e210a98cb645d804b1466be134
Kai Angulo <kangriebling@gmail.com>
2024-06-11 00:18:52 -0700

    Added test app

---
+++ Prowl.Test/Program.cs
@@ -0,0 +1,41 @@
+﻿using Prowl.Runtime;
+using Prowl.Runtime.SceneManagement;
+using Prowl.Runtime.Utils;
+
+namespace Prowl.Standalone;
+
+internal static class Program {
+
+    public static DirectoryInfo Data => new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
+    static Texture2D catTex;
+
+    public static int Main(string[] args) {
+
+        Application.isPlaying = true;
+        Application.DataPath = Data.FullName;
+
+        Application.Initialize += () => {
+            catTex = Texture2DLoader.FromFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Cat.png"));
+        };
+
+        Application.Update += () => {
+
+        };
+
+        Application.Render += () => {
+            Graphics.StartFrame(catTex);
+
+            Graphics.DrawNDCQuad();
+
+            Graphics.EndFrame();
+        };
+
+        Application.Quitting += () => {
+        };
+
+        Application.Run("Prowl Editor", 1920, 1080, null, false);
+
+        return 0;
+    }
+    
+}
