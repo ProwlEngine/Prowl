7221afc8766ce1ca0f283357d2fc182085775e06
Abdiel Lopez <a.paperprototype@gmail.com>
2024-06-14 17:58:46 -0600

    Update BallSocketJoint.cs

--- Prowl.Runtime/Components/Physics/BallSocketJoint.cs
+++ Prowl.Runtime/Components/Physics/BallSocketJoint.cs
@@ -1,6 +1,7 @@
 using Prowl.Icons;
 using BepuPhysics.Constraints;
 using BepuPhysics;
+using System.Numerics;
 
 namespace Prowl.Runtime;
 
@@ -9,6 +10,12 @@ namespace Prowl.Runtime;
 public class BallSocketJoint : MonoBehaviour
 {
     [SerializeField] public Rigidbody? connectedBody;
+    public Vector3 jointPosition;
+    public Vector3 connectedBodyJointPosition;
+    public float angularFrequency;
+    public float twiceDampingRatio;
+    public float frequency;
+    public float dampingRatio;
     
     private Rigidbody? rigidbody;
     
@@ -17,8 +24,19 @@ public class BallSocketJoint : MonoBehaviour
     public override void OnEnable()
     {
         if (rigidbody!.BodyHandle == null) return;
+
+        var springSettings = new SpringSettings();
+        springSettings.AngularFrequency = angularFrequency;
+        springSettings.TwiceDampingRatio = twiceDampingRatio;
+        springSettings.Frequency = frequency;
+        springSettings.DampingRatio = dampingRatio;
+
+        var joint = new BallSocket();
+        joint.LocalOffsetA = jointPosition;
+        joint.LocalOffsetB = connectedBodyJointPosition;
+        joint.SpringSettings = springSettings;
         
-        constraintHandle = Physics.Sim!.Solver.Add<BallSocket>(rigidbody.BodyHandle.Value, connectedBody!.BodyHandle!.Value, new BallSocket());
+        constraintHandle = Physics.Sim!.Solver.Add<BallSocket>(rigidbody.BodyHandle.Value, connectedBody!.BodyHandle!.Value, joint);
     }
     
     public override void OnDisable()
