0d409ed1b27e997cd391f4f2557f6e72addfd337
Kai Angulo <kangriebling@gmail.com>
2024-06-16 02:24:44 -0700

    Added resource setting capabilities to material

--- Prowl.Runtime/Rendering/Shaders/Material.cs
+++ Prowl.Runtime/Rendering/Shaders/Material.cs
@@ -7,46 +7,141 @@ namespace Prowl.Runtime
 {
     public sealed class Material : EngineObject
     {
+        public KeywordState Keywords;
         public AssetRef<Shader> Shader;
         public MaterialPropertyBlock PropertyBlock;
 
 
-        public Material()
+        private DeviceBuffer uniformBuffer;
+        private ResourceSet[] resources;
+
+
+        public Material(AssetRef<Shader> shader, MaterialPropertyBlock? properties = null, KeywordState? keywords = null)
         {
-
-        }
-
-        public Material(AssetRef<Shader> shader)
-        {
-            if (shader.AssetID == Guid.Empty) throw new ArgumentNullException(nameof(shader));
-            Shader = shader;
-        }
-
-        public void SetKeyword(string keyword, bool state)
-        {
-            if (state) EnableKeyword(keyword);
-            else DisableKeyword(keyword);
-        }
-
-        public void EnableKeyword(string keyword)
-        {
-            string? key = keyword?.ToUpper().Replace(" ", "").Replace(";", "");
-            if (string.IsNullOrWhiteSpace(key)) return;
-        }
-
-        public void DisableKeyword(string keyword)
-        {
-            string? key = keyword?.ToUpper().Replace(" ", "").Replace(";", "");
-            if (string.IsNullOrWhiteSpace(key)) return;
-        }
-
-
-        public void SetPass(int pass, bool apply = false)
-        {
+            if (shader.Res == null) 
+                throw new ArgumentNullException(nameof(shader));
             
+            Shader = shader;
+            PropertyBlock = properties ?? new();
+            Keywords = keywords ?? KeywordState.Empty;
         }
 
 
+        public void SetPass(CommandList commandList, bool upload = true, int passIndex = 0, PolygonFillMode fill = PolygonFillMode.Solid)
+        {
+            Pass pass = Shader.Res.GetPass(passIndex);
+
+            ResourceCache.PipelineInfo pipeline = ResourceCache.GetPipelineForPass(pass, fillMode: fill);
+
+            commandList.SetPipeline(pipeline.pipeline);
+
+            if (upload)
+                BindResources(commandList, pass, pipeline);
+        }
+
+
+        // Suboptimal solution that recreates every resource set to ensure that textures are properly sent to shader.
+        // When shaders are being compiled by us, we can optimize this so textures go into their own resource set and then we only have to update the uniform buffer.
+        private void BindResources(CommandList commandList, Pass pass, ResourceCache.PipelineInfo info)
+        {
+            if (resources != null)
+            {
+                for (int i = 0; i < resources.Length; i++)
+                    resources[i].Dispose();
+            }
+            
+            Pass.Variant variant = pass.GetVariant(Keywords);
+
+            int bufferSize = 0;
+
+            for (int set = 0; set < variant.resourceSets.Count; set++)
+            {
+                ShaderResource[] resourceSet = variant.resourceSets[set];
+
+                for (int res = 0; res < resourceSet.Length; res++)
+                    bufferSize += Math.Max(0, resourceSet[res].size);
+            }
+
+            if (uniformBuffer == null || uniformBuffer.SizeInBytes != bufferSize)
+                uniformBuffer = Graphics.ResourceFactory.CreateBuffer(new BufferDescription((uint)bufferSize, BufferUsage.UniformBuffer));
+
+            uint bufferOffset = 0;
+
+            resources = new ResourceSet[variant.resourceSets.Count];
+
+            for (int set = 0; set < variant.resourceSets.Count; set++)
+            {
+                ShaderResource[] resourceSet = variant.resourceSets[set];
+
+                ResourceSetDescription description = new ResourceSetDescription();
+                description.Layout = info.description.ResourceLayouts[set];
+                description.BoundResources = new BindableResource[resourceSet.Length];
+
+                for (int res = 0; res < resourceSet.Length; res++)
+                {
+                    ShaderResource resource = resourceSet[res];
+
+                    if (resource.type == ResourceType.Texture || resource.type == ResourceType.Sampler)
+                    {
+                        AssetRef<Texture> tex = PropertyBlock.GetTexture(resource.name).GetValueOrDefault(null);
+
+                        if (tex == null || tex.Res == null)
+                        {
+                            description.BoundResources[res] = null;
+                            continue;
+                        }
+                        
+                        if (resource.type == ResourceType.Texture)
+                            description.BoundResources[res] = tex.Res.TextureView;
+                        else
+                            description.BoundResources[res] = tex.Res.Sampler.InternalSampler;
+                        
+                        continue;
+                    }
+
+                    UpdateBuffer(commandList, resource, bufferOffset);
+
+                    description.BoundResources[res] = new DeviceBufferRange(uniformBuffer, bufferOffset, (uint)resource.size);
+                    bufferOffset += (uint)resource.size;
+                }
+
+                resources[set] = Graphics.ResourceFactory.CreateResourceSet(description);
+
+                commandList.SetGraphicsResourceSet((uint)set, resources[set]);
+            }
+        }
+
+
+        private void UpdateBuffer(CommandList commandList, ShaderResource resource, uint offset)
+        {
+            switch (resource.type)
+            {
+                case ResourceType.Float:    
+                    commandList.UpdateBuffer(uniformBuffer, offset, PropertyBlock.GetFloat(resource.name));      
+                break;
+
+                case ResourceType.Vector2:  
+                    commandList.UpdateBuffer(uniformBuffer, offset, (System.Numerics.Vector2)PropertyBlock.GetVector2(resource.name));    
+                break;
+
+                case ResourceType.Vector3:  
+                    commandList.UpdateBuffer(uniformBuffer, offset, (System.Numerics.Vector3)PropertyBlock.GetVector3(resource.name));    
+                break;
+
+                case ResourceType.Vector4:  
+                    commandList.UpdateBuffer(uniformBuffer, offset, (System.Numerics.Vector4)PropertyBlock.GetVector4(resource.name));    
+                break;
+
+                case ResourceType.Matrix4x4:
+                    commandList.UpdateBuffer(uniformBuffer, offset, PropertyBlock.GetMatrix(resource.name).ToFloat());
+                break;
+            }
+        }
+
+
+        public void SetKeyword(string keyword, bool state) => Keywords.SetKeyword(keyword, state);
+        public void EnableKeyword(string keyword) => Keywords.EnableKeyword(keyword);
+        public void DisableKeyword(string keyword) => Keywords.DisableKeyword(keyword);
 
         public void SetColor(string name, Color value) => PropertyBlock.SetColor(name, value);
         public void SetVector(string name, Vector2 value) => PropertyBlock.SetVector(name, value);
@@ -55,9 +150,9 @@ namespace Prowl.Runtime
         public void SetFloat(string name, float value) => PropertyBlock.SetFloat(name, value);
         public void SetInt(string name, int value) => PropertyBlock.SetInt(name, value);
         public void SetMatrix(string name, Matrix4x4 value) => PropertyBlock.SetMatrix(name, value);
-        public void SetMatrices(string name, System.Numerics.Matrix4x4[] value) => PropertyBlock.SetMatrices(name, value);
         public void SetTexture(string name, Texture value) => PropertyBlock.SetTexture(name, value);
         public void SetTexture(string name, AssetRef<Texture> value) => PropertyBlock.SetTexture(name, value);
+        public void SetMatrices(string name, System.Numerics.Matrix4x4[] value) { }
 
         //public CompoundTag Serialize(string tagName, TagSerializer.SerializationContext ctx)
         //{
@@ -73,5 +168,4 @@ namespace Prowl.Runtime
         //    PropertyBlock = TagSerializer.Deserialize<MaterialPropertyBlock>(value["PropertyBlock"], ctx);
         //}
     }
-
 }
