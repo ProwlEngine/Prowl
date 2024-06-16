16a5325072d99233a18f6bddab8853306fd9aa4e
Wulferis <acer1171090@gmail.com>
2024-06-16 15:39:55 +1000

    Added Enum Drawer back for Inspector, Forgot it

---
+++ Prowl.Editor/Editor/PropertyDrawer/Drawers/Enum_PropertyDrawer.cs
@@ -0,0 +1,35 @@
+﻿using Prowl.Runtime;
+using Prowl.Runtime.GUI;
+using System.Reflection;
+
+namespace Prowl.Editor.PropertyDrawers
+{
+    [Drawer(typeof(Enum))]
+    public class Enum_PropertyDrawer : PropertyDrawer
+    {
+        public override bool OnValueGUI(Gui gui, string ID, Type targetType, ref object? value)
+        {
+            Enum enumValue = (Enum)value;
+            Array values = Enum.GetValues(targetType);
+            int selectedIndex = Array.IndexOf(values, enumValue);
+
+            string[] names = new string[values.Length];
+            for (int i = 0; i < values.Length; i++)
+            {
+                FieldInfo fieldInfo = targetType.GetField(values.GetValue(i).ToString());
+                TextAttribute attribute = fieldInfo.GetCustomAttribute<TextAttribute>();
+                names[i] = attribute != null ? attribute.text : fieldInfo.Name;
+            }
+
+            bool changed = gui.Combo("#_PropID", "#_PropPopupID", ref selectedIndex, names, 0, 0, Size.Percentage(1f), GuiStyle.ItemHeight);
+
+            if (selectedIndex >= 0 && selectedIndex < values.Length)
+            {
+                value = values.GetValue(selectedIndex);
+            }
+
+            return changed;
+        }
+    }
+
+}
