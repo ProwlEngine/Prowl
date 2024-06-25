eba02088dddfe771f71eef58f66d64ab0198d6ef
Kai Angulo <kangriebling@gmail.com>
2024-06-25 00:13:20 -0700

    Added a quality setting array to QualitySettings

--- Prowl.Runtime/Quality.cs
+++ Prowl.Runtime/Quality.cs
@@ -5,30 +5,45 @@ using Prowl.Runtime.RenderPipelines;
 
 namespace Prowl.Runtime
 {
+    public class QualitySetting
+    {
+        public AssetRef<RenderPipeline> RenderPipeline;
+        public bool testSetting1;
+        public float testSetting2;
+    }
+
     [FilePath("QualitySettings.projsetting", FilePathAttribute.Location.Setting)]
     public class QualitySettings : ScriptableSingleton<QualitySettings>
     {
-        public AssetRef<RenderPipeline> RenderPipeline; 
-
+        [SerializeField]
+        public QualitySetting[] Qualities; 
     }
 
     public static class Quality
     {
-        private static QualitySettings[] Qualities;
-
+        private static QualitySetting EmptyQuality = new()
+        {
+            RenderPipeline = null
+        };
 
         private static void EnsureValidQualities()
-        {
+        {   
+            QualitySetting[] Qualities = QualitySettings.Instance.Qualities;
+
             if (Qualities != null && Qualities.Length > 0 && Qualities[0] != null)
                 return;
 
-            var defaultPipeline = Application.AssetProvider.LoadAsset<RenderPipelines.RenderPipeline>("Defaults/DefaultRenderPipeline.scriptobj");
+            /*
+            var defaultPipeline = Application.AssetProvider.LoadAsset<RenderPipeline>("Defaults/DefaultRenderPipeline.scriptobj");
 
             if (defaultPipeline.IsAvailable == false)
                 Debug.LogError($"Missing Default Render Pipeline!");
+            */
 
-            Qualities = [
-                new QualitySettings()
+            RenderPipeline defaultPipeline = null;
+
+            QualitySettings.Instance.Qualities = [
+                new QualitySetting()
                 {
                     RenderPipeline = defaultPipeline,
                 }
@@ -39,14 +54,20 @@ namespace Prowl.Runtime
 
         public static void SetQualityLevel(int qualityLevel)
         {
+            if (Application.isEditor && Application.DataPath == null)
+                return;
+
             EnsureValidQualities();
             QualityLevel = qualityLevel;
         }
 
-        public static QualitySettings GetQualitySettings(int? qualityLevel = null)
+        public static QualitySetting GetQualitySettings(int? qualityLevel = null)
         {
+            if (Application.isEditor && Application.DataPath == null)
+                return EmptyQuality;
+
             EnsureValidQualities();
-            return Qualities[qualityLevel.GetValueOrDefault(QualityLevel)];
+            return QualitySettings.Instance.Qualities[qualityLevel.GetValueOrDefault(QualityLevel)];
         }
     }
 }
