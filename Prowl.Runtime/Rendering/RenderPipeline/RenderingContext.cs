c41e6385eb4d3e21d1a55be0ed14a34dee4bbb25
Kai Angulo <kangriebling@gmail.com>
2024-06-29 00:57:24 -0700

    Moved RenderingContext to CommandBuffer

--- Prowl.Runtime/Rendering/RenderPipeline/RenderingContext.cs
+++ Prowl.Runtime/Rendering/RenderPipeline/RenderingContext.cs
@@ -1,11 +1,36 @@
 using Prowl.Runtime.Utils;
 using Veldrid;
-
+using System.Collections.Generic;
+using System.Linq;
+using System;
 
 namespace Prowl.Runtime.RenderPipelines
 {
     public class RenderingContext
     {
         public Framebuffer TargetFramebuffer;
+
+        private List<RenderingCommand> internalCommandList = new();
+
+
+        public void Submit(CommandBuffer buffer)
+        {
+            internalCommandList.AddRange(buffer.Buffer);
+        }        
+
+        public void Execute()
+        {
+            CommandList commandList = Graphics.GetCommandList();
+
+            RenderState state = new RenderState();
+
+            for (int i = 0; i < internalCommandList.Count; i++)
+                internalCommandList[i].ExecuteCommand(commandList, ref state);
+
+            Graphics.SubmitCommands(commandList, true);
+
+            state.Clear();
+            commandList.Dispose();
+        }
     }
 }
