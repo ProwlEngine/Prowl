19f97e623bd0f1e86f9b33c9ea6d8d2b8eacd99a
Kai Angulo <kangriebling@gmail.com>
2024-06-30 02:28:38 -0700

    Added minor comments

--- Prowl.Runtime/Rendering/CommandBuffer/RenderState.cs
+++ Prowl.Runtime/Rendering/CommandBuffer/RenderState.cs
@@ -5,6 +5,10 @@ using Veldrid;
 
 namespace Prowl.Runtime
 {
+    /// <summary>
+    /// The current rendering state passed to RenderCommand.ExecuteCommand().
+    /// Defines relevant information about keywords, targets, and pipeline state for commands to use.
+    /// </summary>
     public class RenderState
     {
         internal Framebuffer activeFramebuffer;
