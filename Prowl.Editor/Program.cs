a74804b68216d12bde19b85541a97f3ce46f0281
Kai Angulo <kangriebling@gmail.com>
2024-06-30 02:25:57 -0700

    Remved test shader parsing from Program.cs

--- Prowl.Editor/Program.cs
+++ Prowl.Editor/Program.cs
@@ -31,191 +31,6 @@ public static class Program
             // Start with the project window open
             //new OldProjectsWindow();
             new ProjectsWindow();
-
-            string testShader = @"
-Shader ""Example/NewShaderFormat""
-
-Properties
-{
-	// Material property declarations go here
-	_MainTex(""Albedo Map"", TEXTURE2D)
-	_NormalTex(""Normal Map"", TEXTURE2D)
-	_EmissionTex(""Emissive Map"", TEXTURE2D)
-	_SurfaceTex(""Surface Map x:AO y:Rough z:Metal"", TEXTURE2D)
-	_OcclusionTex(""Occlusion Map"", TEXTURE2D)
-
-	_EmissiveColor(""Emissive Color"", COLOR)
-	_EmissionIntensity(""Emissive Intensity"", FLOAT)
-	_MainColor(""Main Color"", COLOR)
-}
-
-// Global state or options applied to every pass. If a pass doesn't specify a value, it will use the ones defined here
-Global
-{
-    Tags { ""SomeShaderID"" = ""IsSomeShaderType"", ""SomeOtherValue"" = ""SomeOtherType"" }
-
-    // ...or custom values
-    Blend
-    {    
-        Src Color InverseSourceAlpha
-        Dest Alpha One
-
-        Mode Alpha ReverseSubtract
-        
-        Mask None
-    }
-
-    // Stencil state
-    Stencil
-    {
-        // Depth write
-        DepthWrite On
-        
-        // Comparison kind
-        DepthTest LessEqual
-
-        Ref 25
-        ReadMask 26
-        WriteMask 27
-
-        Comparison Front Greater
-
-        Pass Front Keep
-        Fail Back Zero
-        ZFail Front Replace
-    }
-
-    // Rasterizer culling mode
-    Cull Back
-
-    // Global includes added to every program
-    GlobalInclude 
-    {
-        // This value would be able to be used in every <Program> block
-        vec4 aDefaultValue = vec4(0.5, 0.25, 0.75, 1.0);
-    }
-}
-
-
-
-Pass ""DefaultPass"" 
-{
-    Tags { ""SomeValue"" = ""CustomPassType"", ""SomeOtherValue"" = ""SomeOtherType"" }
-
-    Blend Override
-
-    Stencil
-    {
-        Ref 5
-        ReadMask 10
-        WriteMask 14
-
-        Comparison Back Greater
-        Comparison Front LessEqual
-
-        Pass Front IncrementWrap
-        Pass Back Replace
-
-        ZFail Front Zero
-    }
-
-    DepthWrite Off
-    DepthTest Never
-    Cull Off
-
-    // Program vertex stage example
-    Program Vertex
-    {
-        layout (location = 0) in vec3 vertexPosition;
-		layout (location = 0) out vec4 someColor; 
-		
-		void main()
-		{
-			someColor = aDefaultValue;
-            gl_Position = vertexPosition;
-		}
-    }
-
-    // Program fragment stage example
-	Program Fragment
-    {
-        layout (location = 0) in vec4 someColor; 
-    	layout (location = 0) out vec4 outColor; 
-		
-		void main()
-		{
-			outColor = someColor;
-		}
-	}
-}
-
-Pass ""AnotherPass""
-{
-    Tags { ""SomeValue"" = ""CustomPassType"" }
-
-    Blend
-    {
-        Src Alpha InverseSourceColor
-        Src Color DestinationColor
-
-        Dest Alpha One
-        Dest Color InverseBlendFactor
-
-        Mode Alpha Maximum
-        Mode Color ReverseSubtract
-        
-        Mask RGB
-    }
-
-    DepthWrite On
-    DepthTest LessEqual
-    Cull Back
-
-    // Program vertex stage example
-    Program Vertex
-    {
-        layout (location = 0) in vec3 vertexPosition;
-
-		layout (location = 0) out vec4 someColor; 
-		
-		void main()
-		{
-            #if MY_KEYWORD == 0
-
-            #elif
-
-			someColor = aDefaultValue;
-            gl_Position = vertexPosition;
-		}
-    }
-
-    // Program fragment stage example
-	Program Fragment
-    {
-        layout (location = 0) in vec4 someColor; 
-
-    	layout (location = 0) out vec4 outColor; 
-		
-		void main()
-		{
-			outColor = someColor;
-		}
-	}
-}
-
-// If a pass doesn't compile, the whole shader is invalidated. Use a fallback replacement for the entire shader in that case.
-// While per-pass fallbacks would be nice, there's no guarantee that the pass will always have a name or the correct index 
-Fallback ""Fallback/TestShader""
-";
-
-            // Remove Comments
-            // Remove single-line comments
-            testShader = Regex.Replace(testShader, @"//.*", "");
-
-
-            Prowl.Editor.VeldridShaderParser.VeldridShaderParser parser = new(testShader);
-            var shader = parser.Parse();
-            Console.WriteLine(shader.ToString());
         };
 
         Application.Update += () =>
@@ -250,9 +65,9 @@ Fallback ""Fallback/TestShader""
                     // and finally hierarchy on top of asset tree
                     var hierarchy = EditorGuiManager.DockWindowTo(new HierarchyWindow(), assettree, Docking.DockZone.Top, 0.65f);
 
-                    //new ProjectSettingsWindow();
-                    //new PreferencesWindow();
-                    //new AssetSelectorWindow(typeof(Texture2D), (guid, fileid) => {  });
+                    // new ProjectSettingsWindow();
+                    // new PreferencesWindow();
+                    // new AssetSelectorWindow(typeof(Texture2D), (guid, fileid) => {  });
                 }
 
                 Application.DataPath = Project.ProjectDirectory;
@@ -355,7 +170,7 @@ Fallback ""Fallback/TestShader""
             }
             else
             {
-                Runtime.Debug.LogError("Cannot reload assemblies, No project loaded.");
+                Debug.LogError("Cannot reload assemblies, No project loaded.");
             }
         }
     }
