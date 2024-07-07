054f899a8f3bb198242e0d0564ff8193a60d1133
Kai Angulo <kangriebling@gmail.com>
2024-07-07 02:19:00 -0700

    Removed debug log

--- Prowl.Runtime/Rendering/CommandBuffer/Commands/SetMaterialCommand.cs
+++ Prowl.Runtime/Rendering/CommandBuffer/Commands/SetMaterialCommand.cs
@@ -19,8 +19,6 @@ namespace Prowl.Runtime
             state.pipelineSettings.pass = Material.Shader.Res.GetPass(Pass);
             state.pipelineSettings.variant = state.pipelineSettings.pass.GetVariant(keys);
 
-            Debug.Log($"Got variant with keywords: {state.pipelineSettings.variant.VariantKeywords}");
-
             Pipeline pipeline = PipelineCache.GetPipelineForDescription(state.pipelineSettings);
             PipelineCache.GetDescriptionForPipeline(pipeline, out _);
 
