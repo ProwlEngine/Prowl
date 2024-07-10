fe6acf4b98711e20d492064bb7a56d6c8bf12067
Wulferis <acer1171090@gmail.com>
2024-07-10 14:20:14 +1000

    Woops, One of these have to be public to inherit scriptableobject

--- Prowl.Runtime/Resources/ScriptableObject.cs
+++ Prowl.Runtime/Resources/ScriptableObject.cs
@@ -2,7 +2,7 @@
 {
     public abstract class ScriptableObject : EngineObject, ISerializationCallbackReceiver
     {
-        private ScriptableObject() : base() { }
+        public ScriptableObject() : base() { }
         private ScriptableObject(string name) : base(name) { }
 
         // ScriptableObjects can only be created via the AssetDatabase loading them, so their guranteed to always Deserialize
