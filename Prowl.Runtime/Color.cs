79b938aed74c23db5ef377a3a874e153b40cb7cf
Wulferis <acer1171090@gmail.com>
2024-07-13 17:58:58 +1000

    Added Color.FromHSV

--- Prowl.Runtime/Color.cs
+++ Prowl.Runtime/Color.cs
@@ -100,6 +100,69 @@ namespace Prowl.Runtime
             return new Color(a.r + (b.r - a.r) * t, a.g + (b.g - a.g) * t, a.b + (b.b - a.b) * t, a.a + (b.a - a.a) * t);
         }
 
+        // Source: https://gist.github.com/doomlaser/c7b894fa4936297195a053eda21fc0a0
+        public static Color FromHSV(float h, float s, float v, float a = 1)
+        {
+            // no saturation, we can return the value across the board (grayscale)
+            if (s == 0)
+                return new Color(v, v, v, a);
+
+            // which chunk of the rainbow are we in?
+            float sector = h / 60;
+
+            // split across the decimal (ie 3.87 into 3 and 0.87)
+            int i = (int)sector;
+            float f = sector - i;
+
+            float p = v * (1 - s);
+            float q = v * (1 - s * f);
+            float t = v * (1 - s * (1 - f));
+
+            // build our rgb color
+            Color color = new Color(0, 0, 0, a);
+
+            switch (i)
+            {
+                case 0:
+                    color.r = v;
+                    color.g = t;
+                    color.b = p;
+                    break;
+
+                case 1:
+                    color.r = q;
+                    color.g = v;
+                    color.b = p;
+                    break;
+
+                case 2:
+                    color.r = p;
+                    color.g = v;
+                    color.b = t;
+                    break;
+
+                case 3:
+                    color.r = p;
+                    color.g = q;
+                    color.b = v;
+                    break;
+
+                case 4:
+                    color.r = t;
+                    color.g = p;
+                    color.b = v;
+                    break;
+
+                default:
+                    color.r = v;
+                    color.g = p;
+                    color.b = q;
+                    break;
+            }
+
+            return color;
+        }
+
         public static Color operator +(Color a, Color b) => new Color(a.r + b.r, a.g + b.g, a.b + b.b, a.a + b.a);
 
         public static Color operator /(Color a, float b) => new Color(a.r / b, a.g / b, a.b / b, a.a / b);
