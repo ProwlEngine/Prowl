dc338e985800f2a00032ecfec6d7c0596fc57269
Wulferis <acer1171090@gmail.com>
2024-07-16 17:38:46 +1000

    Added LogNode

---
+++ Prowl.Runtime/Utils/NodeSystem/Nodes/General/LogNode.cs
@@ -0,0 +1,39 @@
+﻿namespace Prowl.Runtime.NodeSystem
+{
+    [Node("General/Log")]
+    public class LogNode : InOutFlowNode
+    {
+        public override string Title => "Log";
+        public override float Width => 150;
+
+        [Input] public string Message;
+
+        public enum LogType
+        {
+            Log,
+            Warning,
+            Error
+        }
+        public LogType Type;
+
+        public override void Execute(NodePort input)
+        {
+            var message = GetInputValue<string>("Message", Message);
+
+            switch (Type)
+            {
+                case LogType.Log:
+                    Debug.Log(message);
+                    break;
+                case LogType.Warning:
+                    Debug.LogWarning(message);
+                    break;
+                case LogType.Error:
+                    Debug.LogError(message);
+                    break;
+            }
+
+            ExecuteNext();
+        }
+    }
+}
