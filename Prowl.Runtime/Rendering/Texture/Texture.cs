e632ee77be312b79f1516f57027d019590e85beb
Kai Angulo <kangriebling@gmail.com>
2024-09-16 16:32:39 -0700

    Ensured internal texture name matched texture name

--- Prowl.Runtime/Rendering/Texture/Texture.cs
+++ Prowl.Runtime/Rendering/Texture/Texture.cs
@@ -125,6 +125,7 @@ public abstract class Texture : EngineObject, ISerializable
             throw exception;
 
         InternalTexture = Graphics.Factory.CreateTexture(description);
+        InternalTexture.Name = Name;
 
         TextureViewDescription viewDescription = new()
         {
