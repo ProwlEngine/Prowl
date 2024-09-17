87d4e122abf00bfdec78464c9c9de34363c4c65f
Kai Angulo <kangriebling@gmail.com>
2024-09-17 14:46:48 -0700

    Fixed DX11 GUI shader texture registers

--- Prowl.Runtime/EmbeddedResources/GUI/HLSL/gui-frag.hlsl
+++ Prowl.Runtime/EmbeddedResources/GUI/HLSL/gui-frag.hlsl
@@ -5,8 +5,8 @@ struct PS_INPUT
     float2 uv  : TEXCOORD0;
 };
 
-Texture2D FontTexture : register(t0);
-sampler FontSampler : register(s0);
+Texture2D FontTexture : register(t2);
+sampler FontSampler : register(s1);
 
 float4 FS(PS_INPUT input) : SV_Target
 {
