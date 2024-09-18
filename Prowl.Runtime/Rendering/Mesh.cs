a6ef2c847450c1123bc9d97fab4b3daa71b3904f
Kai Angulo <kangriebling@gmail.com>
2024-09-17 21:18:20 -0700

    Added missing dispose to end of Mesh

--- Prowl.Runtime/Rendering/Mesh.cs
+++ Prowl.Runtime/Rendering/Mesh.cs
@@ -305,6 +305,8 @@ public class Mesh : EngineObject, ISerializable, IGeometryDrawData
         }
 
         Graphics.SubmitCommandList(list, false);
+
+        list.Dispose();
     }
 
     public void SetDrawData(CommandList commandList, ShaderPipeline pipeline)
