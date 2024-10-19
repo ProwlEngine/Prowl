cbf110acb248f09182044e36c9cb7392d3f9aefe
Wulferis <acer1171090@gmail.com>
2024-10-19 15:28:47 +1100

    Inline Attribute for Mathf.ApproximatelyEquals

--- Prowl.Runtime/Math/Mathf.cs
+++ Prowl.Runtime/Math/Mathf.cs
@@ -8,7 +8,7 @@ namespace Prowl.Runtime;
 
 public static class Mathf
 {
-    public static bool ApproximatelyEquals(float value1, float value2) => Math.Abs(value1 - value2) < float.Epsilon;
+    [MethodImpl(MathD.IN)] public static bool ApproximatelyEquals(float value1, float value2) => Math.Abs(value1 - value2) < float.Epsilon;
     [MethodImpl(MathD.IN)] public static bool ApproximatelyEquals(System.Numerics.Vector2 a, System.Numerics.Vector2 b) => ApproximatelyEquals(a.X, b.X) && ApproximatelyEquals(a.Y, b.Y);
     [MethodImpl(MathD.IN)] public static bool ApproximatelyEquals(System.Numerics.Vector3 a, System.Numerics.Vector3 b) => ApproximatelyEquals(a.X, b.X) && ApproximatelyEquals(a.Y, b.Y) && ApproximatelyEquals(a.Z, b.Z);
     [MethodImpl(MathD.IN)] public static bool ApproximatelyEquals(System.Numerics.Vector4 a, System.Numerics.Vector4 b) => ApproximatelyEquals(a.X, b.X) && ApproximatelyEquals(a.Y, b.Y) && ApproximatelyEquals(a.Z, b.Z) && ApproximatelyEquals(a.W, b.W);
