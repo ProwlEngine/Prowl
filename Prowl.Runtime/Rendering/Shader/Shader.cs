0c6754ec9eafe97f11d6be6abac7f7663c70d1f1
Kai Angulo <kangriebling@gmail.com>
2024-10-19 22:21:45 -0700

    style: applied style recommendations to Shader.cs

--- Prowl.Runtime/Rendering/Shader/Shader.cs
+++ Prowl.Runtime/Rendering/Shader/Shader.cs
@@ -31,8 +31,8 @@ public sealed class Shader : EngineObject, ISerializationCallbackReceiver
 
     public Shader(string name, ShaderProperty[] properties, ShaderPass[] passes) : base(name)
     {
-        this._properties = properties;
-        this._passes = passes;
+        _properties = properties;
+        _passes = passes;
 
         OnAfterDeserialize();
     }
@@ -45,7 +45,7 @@ public sealed class Shader : EngineObject, ISerializationCallbackReceiver
                 throw new InvalidOperationException($"Pass with name {pass.Name} conflicts with existing pass at index {_nameIndexLookup[pass.Name]}. Ensure no two passes have equal names.");
         }
 
-        foreach (var pair in pass.Tags)
+        foreach (KeyValuePair<string, string> pair in pass.Tags)
         {
             if (string.IsNullOrWhiteSpace(pair.Key))
                 continue;
@@ -97,105 +97,5 @@ public sealed class Shader : EngineObject, ISerializationCallbackReceiver
     {
         for (int i = 0; i < _passes.Length; i++)
             RegisterPass(_passes[i], i);
-    }
-
-    public string GetStringRepresentation()
-    {
-        StringBuilder builder = new();
-
-        builder.Append($"Shader \"{Name}\"\n\n");
-
-        builder.Append("Properties\n{\n");
-
-        foreach (ShaderProperty property in Properties)
-        {
-            builder.Append($"\t{property.Name}(\"{property.DisplayName}\", {property.PropertyType})\n");
-        }
-
-        builder.Append("}\n\n");
-
-        foreach (ShaderPass pass in Passes)
-        {
-            builder.Append($"Pass {pass.Name}\n{{\n");
-
-            builder.Append("\tTags { ");
-            foreach (var pair in pass.Tags)
-            {
-                builder.Append($"\"{pair.Key}\" = \"{pair.Value}\", ");
-            }
-            builder.Append("}\n\n");
-
-            builder.Append("\tFeatures \n\t{\n");
-            foreach (var pair in pass.Keywords)
-            {
-                if (string.IsNullOrWhiteSpace(pair.Key))
-                    continue;
-
-                builder.Append($"\t\t{pair.Key} [ {string.Join(" ", pair.Value)} ]\n");
-            }
-            builder.Append("\t}\n\n");
-
-            builder.Append($"\tZTest {pass.DepthClipEnabled}\n\n");
-
-            builder.Append($"\tCull {pass.CullMode}\n\n");
-
-            if (pass.Blend.AttachmentStates[0].BlendEnabled)
-            {
-                BlendAttachmentDescription desc = pass.Blend.AttachmentStates[0];
-
-                builder.Append("\tBlend\n\t{\n");
-                builder.Append($"\t\tMode Alpha {desc.AlphaFunction}\n");
-                builder.Append($"\t\tMode Color {desc.ColorFunction}\n\n");
-                builder.Append($"\t\tSrc Alpha {desc.SourceAlphaFactor}\n");
-                builder.Append($"\t\tSrc Color {desc.SourceColorFactor}\n\n");
-                builder.Append($"\t\tDest Alpha {desc.DestinationAlphaFactor}\n");
-                builder.Append($"\t\tDest Color {desc.DestinationColorFactor}\n\n");
-
-                builder.Append($"\t\tMask {desc.ColorWriteMask}\n");
-                builder.Append("\t}\n\n");
-            }
-
-            builder.Append("\tDepthStencil\n\t{\n");
-
-            DepthStencilStateDescription dDesc = pass.DepthStencilState;
-
-            if (dDesc.DepthTestEnabled)
-                builder.Append($"\t\tDepthTest {dDesc.DepthComparison}\n");
-
-            builder.Append($"\t\tDepthWrite {dDesc.DepthWriteEnabled}\n\n");
-
-            builder.Append($"\t\tComparison {dDesc.StencilFront.Comparison} {dDesc.StencilBack.Comparison}\n");
-            builder.Append($"\t\tDepthFail {dDesc.StencilFront.DepthFail} {dDesc.StencilBack.DepthFail}\n");
-            builder.Append($"\t\tFail {dDesc.StencilFront.Fail} {dDesc.StencilBack.Fail}\n");
-            builder.Append($"\t\tPass {dDesc.StencilFront.Pass} {dDesc.StencilBack.Pass}\n\n");
-
-            builder.Append($"\t\tReadMask {dDesc.StencilReadMask}\n");
-            builder.Append($"\t\tRef {dDesc.StencilReference}\n");
-            builder.Append($"\t\tWriteMask {dDesc.StencilWriteMask}\n");
-
-            builder.Append("\t}\n");
-
-            builder.Append("\tInputs\n\t{\n");
-
-            builder.Append("\t\tVertexInputs\n\t\t{\n");
-            foreach (var input in pass.GetVariant(KeywordState.Empty).VertexInputs)
-            {
-                builder.Append($"\t\t\t{input.semantic}\n");
-            }
-            builder.Append("\t\t}\n\n");
-
-            builder.Append("\t\tUniforms\n\t\t{\n");
-            foreach (var uniform in pass.GetVariant(KeywordState.Empty).Uniforms)
-            {
-                builder.Append("\t\t\t" + uniform.ToString().Replace("\n", "\n\t\t\t"));
-            }
-            builder.Append("\n\t\t}\n");
-
-            builder.Append("\t}\n");
-
-            builder.Append("}\n\n");
-        }
-
-        return builder.ToString();
     }
 }
