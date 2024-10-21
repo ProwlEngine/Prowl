18fdee1b8f4707bc0b7ad5cfe69d16c5c1228a49
Wulferis <acer1171090@gmail.com>
2024-10-22 02:33:16 +1100

    Get Euler, Rotation, Layer, Name, Parent, Position, Scale, Tag Nodes

---
+++ Prowl.Runtime/Utils/NodeSystem/Nodes/GameObject/GetPositionNode.cs
@@ -0,0 +1,21 @@
+﻿// This file is part of the Prowl Game Engine
+// Licensed under the MIT License. See the LICENSE file in the project root for details.
+
+namespace Prowl.Runtime.NodeSystem;
+
+[Node("GameObject/Get Position")]
+public class GetPositionNode : Node
+{
+    public override bool ShowTitle => true;
+    public override string Title => "Get Position";
+    public override float Width => 100;
+
+    [Input(ShowBackingValue.Never)] public GameObject Target;
+    [Output] public Vector3 Position;
+
+    public override object GetValue(NodePort input)
+    {
+        GameObject t = GetInputValue("Target", Target);
+        return t != null ? t.Transform.position : null;
+    }
+}
---
+++ Prowl.Runtime/Utils/NodeSystem/Nodes/GameObject/GetScaleNode.cs
@@ -0,0 +1,21 @@
+﻿// This file is part of the Prowl Game Engine
+// Licensed under the MIT License. See the LICENSE file in the project root for details.
+
+namespace Prowl.Runtime.NodeSystem;
+
+[Node("GameObject/Get Scale")]
+public class GetScaleNode : Node
+{
+    public override bool ShowTitle => true;
+    public override string Title => "Get Scale";
+    public override float Width => 100;
+
+    [Input(ShowBackingValue.Never)] public GameObject Target;
+    [Output] public Vector3 Scale;
+
+    public override object GetValue(NodePort input)
+    {
+        GameObject t = GetInputValue("Target", Target);
+        return t != null ? t.Transform.localScale : null;
+    }
+}
