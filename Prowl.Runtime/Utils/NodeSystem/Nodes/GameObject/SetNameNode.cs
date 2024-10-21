fc96aac21a3196847efc31241841c13f86536ea9
Wulferis <acer1171090@gmail.com>
2024-10-21 13:07:03 +1100

    SetLayer, Name, Parent & Tag Nodes

---
+++ Prowl.Runtime/Utils/NodeSystem/Nodes/GameObject/SetNameNode.cs
@@ -0,0 +1,28 @@
+﻿// This file is part of the Prowl Game Engine
+// Licensed under the MIT License. See the LICENSE file in the project root for details.
+
+namespace Prowl.Runtime.NodeSystem;
+
+[Node("GameObject/Set Name")]
+public class SetNameNode : InOutFlowNode
+{
+    public override bool ShowTitle => true;
+    public override string Title => "Set Name";
+    public override float Width => 100;
+
+    [Input] public GameObject Target;
+    [Input] public string Name;
+
+    public override void Execute(NodePort input)
+    {
+        GameObject t = GetInputValue("Target", Target);
+        string name = GetInputValue("Name", Name);
+
+        if (t != null)
+        {
+            t.Name = name;
+        }
+
+        ExecuteNext();
+    }
+}
---
+++ Prowl.Runtime/Utils/NodeSystem/Nodes/GameObject/SetTagNode.cs
@@ -0,0 +1,28 @@
+﻿// This file is part of the Prowl Game Engine
+// Licensed under the MIT License. See the LICENSE file in the project root for details.
+
+namespace Prowl.Runtime.NodeSystem;
+
+[Node("GameObject/Set Tag")]
+public class SetTagNode : InOutFlowNode
+{
+    public override bool ShowTitle => true;
+    public override string Title => "Set Tag";
+    public override float Width => 100;
+
+    [Input] public GameObject Target;
+    [Input] public string Tag;
+
+    public override void Execute(NodePort input)
+    {
+        GameObject t = GetInputValue("Target", Target);
+        string tag = GetInputValue("Tag", Tag);
+
+        if (t != null)
+        {
+            t.tag = tag;
+        }
+
+        ExecuteNext();
+    }
+}
