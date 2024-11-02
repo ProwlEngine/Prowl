84274e7e2f191b073779a5b56f1361761b3ecc8f
Wulferis <acer1171090@gmail.com>
2024-11-02 17:02:28 +1100

    Updated PBR.glsl to HLSL

---
+++ Prowl.Editor/EmbeddedResources/DefaultAssets/PBR.hlsl
@@ -0,0 +1,50 @@
+﻿#ifndef PBR_FUNCTIONS
+#define PBR_FUNCTIONS
+
+// ------------------------------------------------------------------------------
+#ifndef MATH_PI
+#define MATH_PI
+static const float PI = 3.14159265359f;
+#endif
+
+// ----------------------------------------------------------------------------
+float DistributionGGX(float3 N, float3 H, float roughness)
+{
+    float a = roughness * roughness;
+    float a2 = a * a;
+    float NdotH = max(dot(N, H), 0.0f);
+    float NdotH2 = NdotH * NdotH;
+    float nom = a2;
+    float denom = (NdotH2 * (a2 - 1.0f) + 1.0f);
+    denom = PI * denom * denom;
+    return nom / max(denom, 0.001f); // Added epsilon to prevent division by zero
+}
+
+// ----------------------------------------------------------------------------
+float GeometrySchlickGGX(float NdotV, float roughness)
+{
+    float r = (roughness + 1.0f);
+    float k = (r * r) / 8.0f;
+    float nom = NdotV;
+    float denom = NdotV * (1.0f - k) + k;
+    return nom / max(denom, 0.001f);
+}
+
+// ----------------------------------------------------------------------------
+float GeometrySmith(float3 N, float3 V, float3 L, float roughness)
+{
+    float NdotV = max(dot(N, V), 0.0f);
+    float NdotL = max(dot(N, L), 0.0f);
+    float ggx2 = GeometrySchlickGGX(NdotV, roughness);
+    float ggx1 = GeometrySchlickGGX(NdotL, roughness);
+    return ggx1 * ggx2;
+}
+
+// ----------------------------------------------------------------------------
+float3 FresnelSchlick(float cosTheta, float3 F0)
+{
+    //return F0 + (1.0f - F0) * pow(1.0f - cosTheta, 5.0f);
+    return F0 + (1.0f - F0) * exp2(-9.28f * cosTheta); // Faster but slightly less accurate version
+}
+
+#endif
