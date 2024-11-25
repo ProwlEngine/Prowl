ffb1a2b5d1899956bb59d0ec5300408bc67736c0
Wulferis <acer1171090@gmail.com>
2024-11-26 02:45:10 +1100

    Fixed Duplicating GameObjects not applying corrent identifier to GameObject

--- Prowl.Editor/Utilities/UndoRedo/Actions/GameObjects/GameObjectActions.cs
+++ Prowl.Editor/Utilities/UndoRedo/Actions/GameObjects/GameObjectActions.cs
@@ -66,10 +66,10 @@ public class CloneGameObjectAction : AbstractAction
         if (go == null)
             throw new InvalidOperationException("Could not find GameObject with identifier: " + GameObject);
 
-        _gameObject = go.DeepClone(new(false));
+        _gameObject = go.DeepClone();
         _parent = go.parent?.Identifier ?? Guid.Empty;
 
-        var clone = _gameObject.DeepClone();
+        var clone = _gameObject.DeepClone(new(false));
         _clone = clone.Identifier;
         GameObject? parent = EngineObject.FindObjectByIdentifier<GameObject>(_parent);
         if (parent != null)
