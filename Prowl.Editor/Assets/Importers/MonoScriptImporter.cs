9932ada8c4b52728698ee6d925af15519a2214a7
Wulferis <acer1171090@gmail.com>
2025-01-25 03:11:03 +1100

    Removed some redundant code in MonoScriptImporter

--- Prowl.Editor/Assets/Importers/MonoScriptImporter.cs
+++ Prowl.Editor/Assets/Importers/MonoScriptImporter.cs
@@ -9,19 +9,10 @@ namespace Prowl.Editor.Assets;
 [Importer("CSharpIcon.png", typeof(MonoScript), ".cs")]
 public class MonoScriptImporter : ScriptedImporter
 {
-    static DateTime lastReload;
-
     public override void Import(SerializedAsset ctx, FileInfo assetPath)
     {
         ctx.SetMainObject(new MonoScript());
 
-        if (lastReload == default)
-            lastReload = DateTime.UtcNow;
-        else if (lastReload.AddSeconds(2) > DateTime.UtcNow)
-            return;
-
         Program.RegisterReloadOfExternalAssemblies();
-
-        lastReload = DateTime.UtcNow;
     }
 }
