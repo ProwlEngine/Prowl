f6128488625efb81b005a9e450bf6428a32ea59d
Abdiel Lopez <48071553+PaperPrototype@users.noreply.github.com>
2025-10-16 01:46:52 -0400

    Added Primitives class

---
+++ Prowl.Runtime/Primitives.cs
@@ -0,0 +1,96 @@
+using System.Runtime.CompilerServices;
+using Prowl.Runtime.Resources;
+using Prowl.Vector;
+
+namespace Prowl.Runtime;
+
+public static class Primitives
+{
+    private static Material _standardMaterial;
+    private static Mesh _cubeMesh;
+    private static Mesh _cylinderMesh;
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static GameObject Cube(string name, Double3 size)
+    {
+        _standardMaterial ??= new Material(Shader.LoadDefault(DefaultShader.Standard));
+        _cubeMesh ??= Mesh.CreateCube(Double3.One);
+
+        // game object
+        var (go, ren) = Cube(name, size, _standardMaterial, _cubeMesh);
+
+        return go;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static GameObject Cube(string name, Double3 size, Material material)
+    {
+        _cubeMesh ??= Mesh.CreateCube(Double3.One);
+
+        // game object
+        var (go, ren) = Cube(name, size, material, _cubeMesh);
+
+        return go;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static (GameObject, MeshRenderer) Cube(string name, Double3 size, Material material, Mesh mesh)
+    {
+        // game object
+        var go = new GameObject(name);
+        go.Transform.localScale = size;
+
+        // visuals
+        var ren = go.AddComponent<MeshRenderer>();
+        ren.Mesh = mesh;
+        ren.Material = material;
+
+        return (go, ren);
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static GameObject PhysicsCube(string name, Double3 size, bool isStatic = false)
+    {
+        _standardMaterial ??= new Material(Shader.LoadDefault(DefaultShader.Standard));
+        _cubeMesh ??= Mesh.CreateCube(Double3.One);
+
+        // game object
+        var (go, ren, rb, col) = PhysicsCube(name, size, _standardMaterial, _cubeMesh, isStatic);
+        rb.IsStatic = isStatic;
+
+        return go;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static GameObject PhysicsCube(string name, Double3 size, Material material, bool isStatic = false)
+    {
+        _cubeMesh ??= Mesh.CreateCube(Double3.One);
+
+        // game object
+        var (go, ren, rb, col) = PhysicsCube(name, size, material, _cubeMesh, isStatic);
+        rb.IsStatic = isStatic;
+
+        return go;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static (GameObject, MeshRenderer, Rigidbody3D, BoxCollider) PhysicsCube(string name, Double3 size, Material material, Mesh mesh, bool isStatic = false)
+    {
+        // game object
+        var go = new GameObject(name);
+        go.Transform.localScale = size;
+
+        // visuals
+        var ren = go.AddComponent<MeshRenderer>();
+        ren.Mesh = mesh;
+        ren.Material = material;
+
+        // physics
+        var rb = go.AddComponent<Rigidbody3D>();
+        rb.IsStatic = isStatic;
+        var col = go.AddComponent<BoxCollider>();
+        // col.Size = size; // <- boxCollider is scaled with gameobject, no need to scale it here
+
+        return (go, ren, rb, col);
+    }
+}
