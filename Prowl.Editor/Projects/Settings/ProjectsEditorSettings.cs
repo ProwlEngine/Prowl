608d31660e8a48e30926a1bde33e4aa05927e12d
Wulferis <acer1171090@gmail.com>
2026-09-03 03:14:11 +1000

    Project Editor Settings now resets selected resolution on reset

--- Prowl.Editor/Projects/Settings/ProjectsEditorSettings.cs
+++ Prowl.Editor/Projects/Settings/ProjectsEditorSettings.cs
@@ -17,9 +17,10 @@ public class ProjectsEditorSettings : ProjectSettingsBase
     public int SelectedResolutionIndex = 0;
 
     public override void ResetToDefaults()
-    {
-        ColorPalette = DefaultColorPalette();
-    }
+        {
+            ColorPalette = DefaultColorPalette();
+            SelectedResolutionIndex = 0;
+        }
 
     public override void OnGUI(Paper paper, float width)
     {
